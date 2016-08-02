c92786f4b40332893fdfec334f465b77c746a265
Vimo <929992114@qq.com>
2016-08-03 00:40:43 +0800

    Integer.add

--- js/bigInteger.js
+++ js/bigInteger.js
@@ -1,6 +1,19 @@
 /**
  * Created by Administrator on 2016/7/28.
  */
+String.prototype.paddingLeft = function (str, len) {
+    if (this.length >= len || !str.length) {
+        return this;
+    } else {
+        var num = Math.ceil((len - this.length) / str.length);
+        var temp = [];
+        for (var i = 0; i < num; i++) {
+            temp[i] = str
+        }
+        return temp.join('').substr(0, len - this.length) + this;
+    }
+};
+
 
 function BigInteger() {
     this.value = (arguments[0] || 0).toString();
@@ -13,7 +26,7 @@ BigInteger.prototype = {
         if (num.constructor != BigInteger)
             num = new BigInteger(num)
 
-        var carry = 0, result = [], long, short, sum, longLen, shortLen, index, diff;
+        var carry = 0, result = [], long, short, sum, longLen;
         if (this.length > num.length) {
             long = this.value;
             short = num.value;
@@ -22,29 +35,30 @@ BigInteger.prototype = {
             long = num.value;
         }
         longLen = long.length;
-        shortLen = short.length;
-        diff = longLen - shortLen;
 
-        for (var i = 0; i < shortLen; i++) {
-            index = shortLen - 1 - i;
-            sum = (+ short[index]) + (+long[diff + index]) + carry;
+        short = short.paddingLeft('0',longLen);
+
+        for (var i = longLen - 1; i >= 0; i--) {
+            sum = (+ short[i]) + (+long[i]) + carry;
             carry = sum < 10 ? 0 : 1;
-            result.push(sum % 10);
+            result.unshift(sum % 10);
         }
-        for (var j = 0; j < diff; j++) {
-            sum = +long[diff - j - 1] + carry;
-            carry = sum < 10 ? 0 : 1;
-            result.push(sum % 10);
-            if (!carry) {
-                return long.substring(0, diff - j) + result.reverse().join('');
-            }
-        }
-        return result.reverse().join('');
+        carry > 0 && result.unshift(carry);
+
+        return result.join('');
     },
     length : function() {
         return this.value.length;
     },
     valueOf : function() {
         return this.value;
+    },
+    _zeroize : function(num) {
+        var zeros = '';
+        for (var i = 0; i < num.length; i++) {
+            zeros += '0'
+        }
+        this.value += zeros;
     }
+
 };
