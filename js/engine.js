c4932bf47c16238cdc507ebc1c1a196b59e91784
vivomo <929992114@qq.com>
2016-08-23 22:35:01 +0800

    no message

---
+++ js/engine.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ui on 2016/8/23.
+ */
+
+
+;(function () {
+    var s = setInterval(function () {
+        if (document.body) {
+            var topLink = document
+        }
+    }, 20)
+})();
