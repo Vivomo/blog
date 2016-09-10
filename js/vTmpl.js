155aaebb407214df537a4937d71731f23f47cec0
vivomo <929992114@qq.com>
2016-09-10 14:34:45 +0800

    vTmpl 1.0

--- js/vTmpl.js
+++ js/vTmpl.js
@@ -4,14 +4,14 @@
 
 ;(function () {
     var reg = /\s*(\w+)\s*(\|\s*(\w+)(\((.*)\))?)?/;
-    var vt = {
+    var vt = window.vTmpl = {
         render : function (str, obj) {
             if (Array.isArray(obj)) {
                 return obj.map(function (item) {
                     return vt.render(str, item)
                 }).join('');
             } else if(obj && (typeof obj == 'object')) {
-                str.replace(/\{\{(.+?)}}/g, function (s, $1, index) {
+                str = str.replace(/\{\{(.+?)}}/g, function (s, $1) {
                     /**
                      *  eg. $1 = a | b("YY", 2) => ["a | b("YY", 2)", "a", "| b("YY", 2)", "b", "("YY", 2)", ""YY", 2"]
                      *  eg. $1 = a | b() => ["a | b()", "a", "| b()", "b", "()", ""]
@@ -21,21 +21,40 @@
                     var filter = result[3];
                     var key = result[1];
                     var args = result[5];
-                    if (filter) {
-                        if (vt.filters[filter]) {
-                            return new Function('return vt.filters[filter](key'+(args === undefined ? '': ',' + args)+')');
+                    if (key in obj) {
+                        if (filter) {
+                            if (vt.filters[filter]) {
+                                var funcBody = 'return vTmpl.filters["'+filter+'"]('+obj[key]+(args === undefined ? '': ',' + args)+')';
+                                return (new Function(funcBody))();
+                            } else {
+                                console.error('The filter of ' + filter + ' is not defined')
+                            }
                         } else {
-                            console.error('The filter of ' + filter + ' is not defined')
+                            return obj[key]
                         }
-                    } else if(key in obj){
-                        return obj[key]
+                    } else {
+                        return s;
                     }
-                    return s;
-
-
                 })
             }
             return str;
+        },
+        filters : {
+            date : function (time, format) {
+                var date = new Date(time);
+                var dateInfo = {
+                    'yyyy' : date.getFullYear(),
+                    'YYYY' : date.getFullYear(),
+                    'MM' : date.getMonth() + 1,
+                    'mm' : date.getMinutes(),
+                    'HH' : date.getHours(),
+                    'hh' : date.getHours() > 13 ? date.getHours() - 12 : date.getHours(),
+                    'dd' : date.getDate()
+                };
+                return format.replace(/\w+/g, function (key) {
+                    return dateInfo[key] || key;
+                })
+            }
         }
     }
 })();
