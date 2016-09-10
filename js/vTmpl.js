a26c1f9d7e5e41084956f2ff4013f9812b0d6ed2
vivomo <929992114@qq.com>
2016-09-10 13:48:53 +0800

    no message

--- js/vTmpl.js
+++ js/vTmpl.js
@@ -3,17 +3,35 @@
  */
 
 ;(function () {
-    var reg = /\{\{\s*(\w+)\s*(\|\s*(\w+)(\((.*)\))?)?}}/;
+    var reg = /\s*(\w+)\s*(\|\s*(\w+)(\((.*)\))?)?/;
     var vt = {
         render : function (str, obj) {
             if (Array.isArray(obj)) {
-                obj.forEach(function (item) {
+                return obj.map(function (item) {
                     return vt.render(str, item)
-                })
+                }).join('');
             } else if(obj && (typeof obj == 'object')) {
                 str.replace(/\{\{(.+?)}}/g, function (s, $1, index) {
+                    /**
+                     *  eg. $1 = a | b("YY", 2) => ["a | b("YY", 2)", "a", "| b("YY", 2)", "b", "("YY", 2)", ""YY", 2"]
+                     *  eg. $1 = a | b() => ["a | b()", "a", "| b()", "b", "()", ""]
+                     *  eg. $1 = a | b  => ["a | b", "a", "| b", "b", undefined, undefined]
+                     */
                     var result = reg.exec($1);
-                    
+                    var filter = result[3];
+                    var key = result[1];
+                    if (filter) {
+                        if (vt.filters[filter]) {
+                            return new Function(key)
+                        } else {
+                            console.error('The filter of ' + filter + ' is not defined')
+                        }
+                    } else if(key in obj){
+                        return obj[key]
+                    }
+                    return s;
+
+
                 })
             }
             return str;
