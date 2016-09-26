6329962d7668f1b26366854cb220a836449d9432
vivomo <929992114@qq.com>
2016-09-26 19:36:06 +0800

    test site data

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ui on 2016/9/26.
+ */
+
+(function () {
+    var dataElem = document.getElementById('data');
+    if (dataElem) {
+        var data = JSON.parse(dataElem.value);
+        if (data.js) {
+            // V.loadJS()
+        }
+    }
+})();
