4de0c3a1c528037eab24881b22f5ae782b29b83e
vivomo <929992114@qq.com>
2016-09-29 19:26:50 +0800

    printer  完善

--- js/printer.js
+++ js/printer.js
@@ -1,22 +1,22 @@
 /**
  * Created by ui on 2016/9/29.
  */
-
+function extend(o1, o2) {
+    for (var k in o2) {
+        o1[k] = o2[k]
+    }
+    return o1;
+}
 function Printer(cfg) {
 
     if (this.constructor != Printer) {
         return new Printer(cfg)
     }
 
-    function extend(o1, o2) {
-        for (var k in o2) {
-            o1[k] = o2[k]
-        }
-        return o1;
-    }
     extend(this, cfg);
 
     this.queue = [];
+    this.parent = this.cursor.parentNode;
 
     if (this.audioId) {
         this.audio = document.getElementById(this.audioId);
@@ -40,8 +40,10 @@ Printer.prototype = {
     tag : '',
     isWriting : false,
     start : function () {
+        this.cursor.style.display = 'inline-block';
+        this.isWriting = true;
         this.audio && this.audio.play();
-        var interval = setInterval(function () {
+        this.interval = setInterval(function () {
             var next = this.queue.shift();
             if (next) {
                 switch (typeof next){
@@ -52,23 +54,34 @@ Printer.prototype = {
                         next();
                 }
             } else {
-                clearInterval(interval);
-                this.cursor.style.display = 'none';
+                this.end();
             }
 
         }.bind(this), this.pauseTime)
     },
+    end : function () {
+        clearInterval(this.interval);
+        this.cursor.style.display = 'none';
+        this.isWriting = false;
+        this.audio && this.audio.pause();
+    },
     switchCursor : function (cursor) {
-        this.cursor && (this.cursor.style.display = 'none');
-        // this.audio && this.audio.pause();
-
+        this.cursor.style.display = 'none';
         this.cursor = cursor;
-        cursor.style.display = 'inline-block';
+        this.cursor.style.display = 'inline-block';
     },
     write : function (cursor, word, callback) {
-        this.queue = this.queue.concat(function(){
-            this.switchCursor(cursor);
-        }.bind(this),word.split(''), callback);
+        var queue = [];
+        if (typeof cursor == 'object') {
+            queue = queue.concat(function () {
+                this.switchCursor(cursor);
+            }, word.split(''));
+        } else {
+            queue = queue.concat(cursor.split(''));
+            callback = word;
+        }
+        callback && queue.push(callback);
+        this.queue = this.queue.concat(queue);
 
         if (!this.isWriting) {
             this.start();
@@ -76,7 +89,19 @@ Printer.prototype = {
         return this;
     },
     pushLetter : function (letter) {
-        this.cursor.parentNode.insertBefore(this.createElement(letter), this.cursor);
+        this.parent.insertBefore(this.createElement(letter), this.cursor);
     }
-
 };
+
+
+// test
+
+var s2 = 'concat 方法并不修改调用它的对象(this 指向的对象) 和参数中的各个数组本身的值,而是将他们的每个元素拷贝一份放在组合成的新数组中.原数组中的元素有两种被拷贝的方式:';
+var s1='concat 方法将创建一个新的数组，然后将调用它的对象(this 指向的对象)中的元素以及所有参数中的数组类型的参数中的元素以及非数组类型的参数本身按照顺序放入这个新数组,并返回该数组';
+
+var p = new Printer({
+    cursor : document.querySelector('.cursor')
+});
+
+p.write(s1).write(s2);
+
