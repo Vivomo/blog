21d3f2acd032afb9e71f6fc20da04869c50b0386
vivomo <929992114@qq.com>
2016-11-25 20:10:09 +0800

    calendar

---
+++ js/calendar.js
@@ -0,0 +1,21 @@
+(function () {
+    var calendar = window.Calendar = function (option) {
+
+    };
+
+    calendar.prototype = {
+        constructor : calendar,
+        start : 1990,
+        end : 2099,
+        prev : function () {
+
+        },
+        next : function () {
+
+        },
+
+        init : function () {
+
+        }
+    }
+})();
