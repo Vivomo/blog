c0dd7a40a6122a9ea5897f1c52811bb3bdfe564d
vivomo <929992114@qq.com>
2016-11-28 16:14:12 +0800

    Calendar.prototype 减负

--- js/calendar.js
+++ js/calendar.js
@@ -10,6 +10,8 @@
         }
     };
 
+    var todayStyleTemplate = '<style id="currentDay-style">.className{background:rgba(43, 170, 177, 0.6)}</style>'
+
     var monthHTML = '';
     var Calendar = window.Calendar = function (option) {
         $.extend(this, option);
@@ -114,7 +116,6 @@
                 calendar.setViewModel(calendar.viewModel - 1);
             });
 
-
         },
         initMonth : function () {
             monthHTML = language[this.language].month.map(function (month) {
@@ -168,9 +169,9 @@
                     yearOfNextMonth = month == 11 ? year + 1 : year,
                     prevMonth = (month + 11) % 12;
 
-                var weekOFMonth1st = this.getWeek(year, month + 1, 1);
-                var totalDay = this.getTotalDayOFMonth(year, month);
-                var prevMonthLastDay = this.getTotalDayOFMonth(yearOfPrevMonth, prevMonth);
+                var weekOFMonth1st = getWeek(year, month + 1, 1);
+                var totalDay = getTotalDayOFMonth(year, month);
+                var prevMonthLastDay = getTotalDayOFMonth(yearOfPrevMonth, prevMonth);
 
                 var html = '', w = 0, i;
                 var prevPatchDay = weekOFMonth1st || 7;
@@ -192,7 +193,6 @@
                         date : i
                     });
                 }
-
                 var nextPatchDay = 6 - w % 7 + 1 + (w < 35 ? 7 : 0);
                 for (i = 1; i <= nextPatchDay; i++) {
                     html += this.createDateHtml({
@@ -207,44 +207,7 @@
             }
         },
         createCurrentDateStyle : function (year, month, day) {
-            var style = '.date-'+year+'-'+month+'-'+day + '{' + 'background:rgba(43, 170, 177, 0.6)}';
-            $('head').append('<style id="currentDay-style">'+style+'</style>');
-        },
-        /**
-         * 蔡勒公式中1月用13表示，2月用14表示, 同时年-1
-         * (不直接new Date.getDay 为了性能)
-         * https://zh.wikipedia.org/wiki/%E8%94%A1%E5%8B%92%E5%85%AC%E5%BC%8F
-         * @param year
-         * @param month
-         * @param day
-         * @returns {number}
-         */
-        getWeek : function(year, month, day) {
-            if (month < 3) {
-                month += 12;
-                year -= 1;
-            }
-            var century = ~~ (year / 100);
-            year = year % 100;
-            return Math.abs( (year + ~~(year/4) + ~~(century/4) - 2 * century + ~~(26 * (month + 1) / 10) + day - 1)
-                % 7);
-        },
-        getTotalDayOFMonth : function(year, month) {
-            switch (month+1) {
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    return 30;
-                case 2:
-                    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) ) {
-                        return 29;
-                    } else {
-                        return 28;
-                    }
-                default :
-                    return 31;
-            }
+            $('head').append(todayStyleTemplate.replace('className', '.date-'+year+'-'+month+'-'+day));
         },
         createDateHtml : function (option) {
             var className = (option.className || '') + ' w' + option.w +
@@ -259,31 +222,41 @@
         }
     };
 
-
-    Calendar.currentDate = (function(){
-        var now = new Date();
-        var millisecondsOfDay = 86400000;
-        return {
-            year : now.getFullYear(),
-            month : now.getMonth() + 1,
-            date : now.getDate(),
-            time : now.getTime(),
-            equals : function(time) {
-                var date;
-                if (typeof time == 'string' || typeof time == 'number') {
-                    date = new Date(time)
-                } else if(typeof time == 'object' && time.constructor == 'Date') {
-                    date = time;
+     function getTotalDayOFMonth(year, month) {
+        switch (month+1) {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) ) {
+                    return 29;
                 } else {
-                    return false;
+                    return 28;
                 }
-                return ~~ (now.getTime() / millisecondsOfDay) == ~~ (date.getTime() / millisecondsOfDay);
-            }
+            default :
+                return 31;
         }
-    })();
+    }
+    /**
+     * 蔡勒公式中1月用13表示，2月用14表示, 同时年-1
+     * (不直接new Date.getDay 为了性能)
+     * https://zh.wikipedia.org/wiki/%E8%94%A1%E5%8B%92%E5%85%AC%E5%BC%8F
+     * @param year
+     * @param month
+     * @param day
+     * @returns {number}
+     */
+    function getWeek(year, month, day) {
+        if (month < 3) {
+            month += 12;
+            year -= 1;
+        }
+        var century = ~~ (year / 100);
+        year = year % 100;
+        return Math.abs( (year + ~~(year/4) + ~~(century/4) - 2 * century + ~~(26 * (month + 1) / 10) + day - 1)
+            % 7);
+    }
 
-
-    new Calendar({
-
-    });
 })();
