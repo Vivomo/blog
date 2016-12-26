1a035b795f6694d0a50970e4264b4ad9a6c37694
vivomo <929992114@qq.com>
2016-12-26 16:59:35 +0800

    page1 finish

--- js/report.js
+++ js/report.js
@@ -1,15 +1,38 @@
-var $ = function (selector) {
-    return document.querySelector(selector);
-};
+
 
 var $$ = function (selector) {
     return [].slice.call(document.querySelectorAll(selector));
 };
 var Report = (function () {
+    var $page = $('.page'),
+        $clubName = $('#club-name'),
+        $start = $('#start');
+
+    function animated($elem) {
+        $elem.addClass($elem.data('animate'));
+    }
 
     return {
+        initedSections : [],
+        start : function () {
+            animated($clubName);
+            setTimeout(function () {
+                animated($start);
+            }, 1000);
+        },
+        initSection : function (name) {
+            if (this.initedSections.indexOf(name) == -1) {
+                this[name] && this[name]();
+                this.initedSections.push(name);
+            }
+        },
         init : function () {
-
+            $('#wrap').fullpage({
+                afterLoad : function (a, index) {
+                    var _$page = $page.eq(index-1);
+                    Report.initSection([_$page.data('name')]);
+                }
+            })
         }
     }
 })();
