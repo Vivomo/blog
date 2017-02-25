b30dc3ed64cf890b1540fc069f6802ed5e4bba7f
Vimo <929992114@qq.com>
2017-02-25 10:54:31 +0800

    chrome 插件 vimo block

---
+++ chrome_plugins/vimo_block/block.js
@@ -0,0 +1,15 @@
+(function () {
+
+    var block = {
+        "coding.imooc.com" : function (){
+            var style = document.createElement('style');
+            style.innerText =  ' .video-js .vjs-tech{ z-index:10000;} ';
+            document.head.appendChild(style);
+        }
+    };
+    var host = location.host;
+
+    if (host in block) {
+        block[host]();
+    }
+})();
