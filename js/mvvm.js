9816006ba91137446830b0f4f2a6fa92be1c0407
Vimo <929992114@qq.com>
2017-03-14 04:01:09 +0800

    贪吃蛇

--- js/mvvm.js
+++ js/mvvm.js
@@ -1,6 +1,23 @@
 (function (root) {
 
-    var v = function () {
-        
+    var v = root.V = {
+        define : function () {
+
+        },
+        scan : function () {
+
+        },
+
+        init : function (cfg) {
+            var vm = {};
+            for (var k in cfg) {
+                Object.defineProperty(vm, k, {
+                    get : function () {
+                        return vm['_'+k]
+                    }
+                })
+            }
+        }
     }
+
 })(window);
--- js/snake.js
+++ js/snake.js
@@ -1,22 +1,59 @@
-(function () {
+//(function () {
     avalon.config({
         interpolate: ["[[","]]"]
     });
+
+    var DIRECTION = {
+        LEFT: 0,
+        TOP: 1,
+        RIGHT: 2,
+        DOWN: 3
+    };
+
+    function nextSquare(head, direction){
+        var x, y;
+        if (direction % 2 == 0) {
+            y = head.y;
+            x = head.x + direction - 1;
+        } else {
+            x = head.x;
+            y = head.y + direction - 2;
+        }
+        return {
+            x: x,
+            y: y
+        }
+    }
+
+
 
     var snake = window.snake = avalon.define({
         $id : 'snake',
         ceilLength: 11,
         body: [],
         square: [],
-        direction : 1, // 0123 依次代表左上右下
-        speed : 500,
+        direction : DIRECTION.TOP, // 0123 依次代表左上右下
+        speed : 1500,
         ceilWidth : 60,
         move : function () {
+
+            var head = snake.body[0];
+            var ceil = nextSquare(head, snake.direction);
+            if (snake.isOutOfIndex(ceil)) {
+                console.log('game over!');
+                snake.stop();
+                return;
+            }
+            if (snake.isFood(ceil)) {
+                snake.eat(ceil);
+                return;
+            }
             for (var i = snake.body.length - 1; i > 0; i--) {
                 snake.body[i].x = snake.body[i-1].x;
                 snake.body[i].y = snake.body[i-1].y;
             }
-            var head = snake.body[0];
+
+
             switch (snake.direction) {
                 case 0:
                     head.x -= 1;
@@ -31,6 +68,18 @@
                     head.y += 1;
                     break;
             }
+
+        },
+        eat: function (food) {
+            snake.body.unshift(food);
+            snake.square[food.y][food.x].isFood = false;
+            snake.makeFood();
+        },
+        isOutOfIndex: function (ceil) {
+            return ceil.x < 0 || ceil.x >= snake.ceilLength || ceil.y < 0 || ceil.y >= snake.ceilLength
+        },
+        isFood: function (ceil) {
+            return snake.square[ceil.y][ceil.x].isFood;
         },
         makeFood : function () {
             while (true) {
@@ -42,7 +91,7 @@
                 });
 
                 if (effective) {
-                    snake.square[x][y].isFood = true;
+                    snake.square[y][x].isFood = true;
                     break;
                 }
             }
@@ -73,27 +122,37 @@
             snake.square = square;
 
             document.addEventListener('keydown', function (e) {
+                if (e.keyCode < 37 || e.keyCode > 40) {
+                    return
+                }
                 var direction = e.keyCode - 37;
                 if (Math.abs(direction - snake.direction) != 2) {
                     snake.direction = direction;
+                    snake.stop();
                     snake.move();
+                    snake.start();
                 }
-            })
+            });
+
+            snake.makeFood();
+            snake.start();
+        },
+        stop : function () {
+            clearInterval(snake.interval);
         },
         start : function () {
-            snake.init();
-            snake.makeFood();
-            setInterval(function () {
+            snake.interval = setInterval(function () {
                 snake.move();
             }, snake.speed);
         }
     });
 
-    snake.start();
+
+    //snake.init();
 
     avalon.scan();
 
 
 
 
-})();
+//})();
