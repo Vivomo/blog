1f9a84e388cb3769863c0a0f912aaed7f42a861d
Vimo <929992114@qq.com>
2017-04-03 02:16:36 +0800

    add compare animate

--- js/sort-3d.js
+++ js/sort-3d.js
@@ -10,6 +10,12 @@
             row: row,
             col: col,
             list: []
+     /*       active: function () {
+                var result = arguments[0];
+                [].slice.apply(arguments).slice(1).every(function(index){
+                    vm.list[]
+                })
+            }*/
         }),
         square = document.querySelector('.square');
 
@@ -21,6 +27,7 @@
                 vm.list = avalon.range(0, vm.col).map(function (index) {
                     return {
                         index: index,
+                        comparing: false,
                         height: ~~(Math.random() * maxHeight)
                     }
                 });
@@ -34,24 +41,46 @@
         var defaultKey = 'height';
         return {
             swap: function (arr, i, j, key) {
+                arr[i].comparing = true;
+                arr[j].comparing = true;
                 if (arr[i][key] - arr[j][key] > 0) {
                     var temp = arr[i].index;
                     arr[i].index =arr[j].index;
                     arr[j].index = temp;
-                    var item = arr[i].$model;
-                    arr.splice(i, 1, arr[j].$model);
-                    arr.splice(j, 1, item);
+
+                    setTimeout(function () {
+                        var item = arr[i].$model;
+                        arr.splice(i, 1, arr[j].$model);
+                        arr.splice(j, 1, item);
+                    }, 300)
                 }
+                setTimeout(function () {
+                    arr[i].comparing = false;
+                    arr[j].comparing = false;
+                }, 300)
             },
 
             bubble: function (arr, key) {
                 key = key || defaultKey;
                 var lastIndex = arr.length - 1;
-                for (var i = 0; i < lastIndex; i++) {
+     /*           for (var i = 0; i < lastIndex; i++) {
                     for (var j = lastIndex; j > i; j--) {
                         this.swap(arr, j - 1, j, key);
                     }
-                }
+                }*/
+                var i = 0,
+                    j = lastIndex;
+                var interval = setInterval(function () {
+                    this.swap(arr, j - 1, j, key);
+                    j --;
+                    if (j == i) {
+                        i++;
+                        j = lastIndex;
+                    }
+                    if (i >= lastIndex) {
+                        clearInterval(interval)
+                    }
+                }.bind(this), 500);
             }
         }
     })();
