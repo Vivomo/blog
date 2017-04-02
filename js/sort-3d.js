54e62e045eab79b1dd8406bb3c375b38b505f816
Vimo <929992114@qq.com>
2017-04-03 01:48:00 +0800

    bubble sort finish

--- js/sort-3d.js
+++ js/sort-3d.js
@@ -1,4 +1,4 @@
-(function () {
+//(function () {
     avalon.config({
         interpolate: ["[[", "]]"]
     });
@@ -16,22 +16,49 @@
     var cube = (function(){
         var maxHeight = 400;
         return {
+
             init: function () {
                 vm.list = avalon.range(0, vm.col).map(function (index) {
                     return {
                         index: index,
                         height: ~~(Math.random() * maxHeight)
                     }
-                })
+                });
 
                 avalon.scan();
             }
         }
     })();
 
-    cube.init()
+    var Sort = (function () {
+        var defaultKey = 'height';
+        return {
+            swap: function (arr, i, j, key) {
+                if (arr[i][key] - arr[j][key] > 0) {
+                    var temp = arr[i].index;
+                    arr[i].index =arr[j].index;
+                    arr[j].index = temp;
+                    var item = arr[i].$model;
+                    arr.splice(i, 1, arr[j].$model);
+                    arr.splice(j, 1, item);
+                }
+            },
+
+            bubble: function (arr, key) {
+                key = key || defaultKey;
+                var lastIndex = arr.length - 1;
+                for (var i = 0; i < lastIndex; i++) {
+                    for (var j = lastIndex; j > i; j--) {
+                        this.swap(arr, j - 1, j, key);
+                    }
+                }
+            }
+        }
+    })();
+
+    cube.init();
 
 
 
 
-})();
+//})();
