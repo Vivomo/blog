39ba02a0cda875f53b9bca311739ef212c3145b3
Vimo <929992114@qq.com>
2017-04-30 05:27:54 +0800

    plan

--- test/es6/es6-shims.js
+++ test/es6/es6-shims.js
@@ -86,5 +86,73 @@
     /**
      * padStart
      * padEnd
+     * String.raw
+     */
+
+    //Number
+
+    /**
+     * Number.
+     *  isFinite
+     *  isNaN
+     *  parseInt
+     *  parseFloat
+     *  isInteger
+     *  isSafeInteger
+     *  EPSILON //2.220446049250313e-16 极小的常量
+     *  MAX_SAFE_INTEGER
+     *  MIN_SAFE_INTEGER
+     */
+
+    /**
+     * Math.
+     *  trunc //去除小数部分
+     *  sign
+     *  cbrt // 立方根
+     *  clz32 返回一个数的32位无符号整数形式有多少个前导0。
+     *  imul方法返回两个数以32位带符号整数形式相乘的结果
+     *  fround方法返回一个数的单精度浮点数形式。
+     *  hypot方法返回所有参数的平方和的平方根。
+     *  expm1 Math.expm1(x)返回ex - 1，即Math.exp(x) - 1。
+     *  log1p Math.log1p(x)方法返回1 + x的自然对数，即Math.log(1 + x)。如果x小于-1，返回NaN。
+     *  log10 Math.log10(x)返回以10为底的x的对数。如果x小于0，则返回NaN。
+     *  log2 返回以2为底的x的对数。如果x小于0，则返回NaN。
+     *
+     *  Math.sinh(x) 返回x的双曲正弦（hyperbolic sine）
+     *  Math.cosh(x) 返回x的双曲余弦（hyperbolic cosine）
+     *  Math.tanh(x) 返回x的双曲正切（hyperbolic tangent）
+     *  Math.asinh(x) 返回x的反双曲正弦（inverse hyperbolic sine）
+     *  Math.acosh(x) 返回x的反双曲余弦（inverse hyperbolic cosine）
+     *  Math.atanh(x) 返回x的反双曲正切（inverse hyperbolic tangent）
+     */
+
+    /**
+     * Array.
+     *  from
+     *  of
+     * Array.prototype.
+     *  copyWithin 在当前数组内部，将指定位置的成员复制到其他位置（会覆盖原有成员），然后返回当前数组。也就是说，使用这个方法，会修改当前数组。
+     *  find
+     *  findIndex
+     *  fill
+     *  findIndex
+     *  keys
+     *  values
+     *  includes
+     */
+
+
+    /**
+     * function.
+     *  name 这个属性早就被浏览器广泛支持，但是直到 ES6，才将其写入了标准。
+     */
+
+    /**
+     * Object.
+     *  is
+     *  assign
+     *  values
+     *  entries
+     *
      */
 })();
