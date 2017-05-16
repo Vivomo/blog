633c48bf26a14221349b50bb57632d6f16fd3448
Vimo <929992114@qq.com>
2017-05-17 01:57:55 +0800

    d3 test

---
+++ js/d3/index.js
@@ -0,0 +1,40 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    g = svg.append("g").attr("transform", "translate(40,0)");
+
+var tree = d3.tree()
+    .size([height, width - 160]);
+
+var stratify = d3.stratify()
+    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+d3.csv("../../src/flare.csv", function(error, data) {
+    if (error) throw error;
+
+    var root = stratify(data)
+        .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
+
+    var link = g.selectAll(".link")
+        .data(tree(root).links())
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", d3.linkHorizontal()
+            .x(function(d) { return d.y; })
+            .y(function(d) { return d.x; }));
+
+    var node = g.selectAll(".node")
+        .data(root.descendants())
+        .enter().append("g")
+        .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+    node.append("circle")
+        .attr("r", 3.5);
+
+    node.append("text")
+        .attr("dy", 3)
+        .attr("x", function(d) { return d.children ? -8 : 8; })
+        .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+        .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+});
