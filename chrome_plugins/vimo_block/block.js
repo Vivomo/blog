141b17d41876c3a118e526402099bbe7ddb39857
Vimo <929992114@qq.com>
2017-06-02 23:39:49 +0800

    vimo block add lol style

--- chrome_plugins/vimo_block/block.js
+++ chrome_plugins/vimo_block/block.js
@@ -1,9 +1,20 @@
 (function () {
 
     var block = {
-        "coding.imooc.com" : function (){
+        "coding.imooc.com" :function (){
             var style = document.createElement('style');
             style.innerText =  ' .next-mask.in, #video-container-mocoplayer-hls-video > [class]{ z-index:1000;background:#000} ';
+            document.head.appendChild(style);
+        },
+        "lpl.qq.com": function () {
+            var style = document.createElement('style');
+            style.innerText = `.data-teamvs .match-score {
+                opacity: 0.001;
+            }
+            .data-teamvs .match-score:hover {
+                opacity: 1;
+            }
+            `;
             document.head.appendChild(style);
         }
     };
