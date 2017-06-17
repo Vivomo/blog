85a8a879d20ba6a16b38f46f92d9a13820ac278c
Vimo <929992114@qq.com>
2017-06-18 00:00:59 +0800

    chrome plugins notifications

--- chrome_plugins/Notifications/notifications.js
+++ chrome_plugins/Notifications/notifications.js
@@ -1,4 +1,9 @@
 (function () {
 
-    
+    setTimeout(function () {
+        new Notification('Title', {
+            body : 'I am a Notification',
+            icon : './images/test1.png'
+        });
+    }, 5000);
 })();
