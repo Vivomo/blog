078714e4f9bc48cada436101ca33ed0c066eb90b
Vimo <929992114@qq.com>
2017-06-21 20:12:40 +0800

    TreeUtil loop

--- js/utils/TreeUtils.js
+++ js/utils/TreeUtils.js
@@ -4,27 +4,40 @@ var TreeUtils = {
     /**
      * 浅克隆tree,
      *
-     * @param root 需要克隆的树
+     * @param tree 需要克隆的树
      * @param cfg {Object} {childrenKey, filter  } 克隆配置, 可以指定childrenKey, filter不需要克隆的属性的数组,
      *          childrenKey 会自动加到filter 当中
      */
-    clone: function (root, cfg) {
+    clone: function (tree, cfg) {
         cfg = cfg || {};
         var childrenKey = cfg.childrenKey || TreeUtils.childrenKey;
         var filterKeys = cfg.filter ? cfg.filter.concat(childrenKey) : [childrenKey];
 
         var _tree = {};
-        Object.keys(root).forEach(function (key) {
+        Object.keys(tree).forEach(function (key) {
             if (!filterKeys.includes(key)) {
-                _tree[key] = root[key];
+                _tree[key] = tree[key];
             }
         });
-        if (root[childrenKey]) {
+        if (tree[childrenKey]) {
             _tree[childrenKey] = [];
-            root[childrenKey].forEach(function (item) {
+            tree[childrenKey].forEach(function (item) {
                 _tree[childrenKey].push(TreeUtils.clone(item));
             });
         }
         return _tree;
+    },
+
+    loop: function (tree, callback) {
+        if (callback(tree) === false) {
+            return;
+        }
+        if (tree.children) {
+            tree.children.forEach(function (item) {
+                loopTree(item, callback);
+            })
+        }
     }
+
+
 };
