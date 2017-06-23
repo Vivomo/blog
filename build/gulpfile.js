e8c2ada79769ef74cafe3486d27cb184ef9878e0
Vimo <929992114@qq.com>
2017-06-23 15:51:03 +0800

    gulp less

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp'),
     less = require('gulp-less');
 
 gulp.task('testLess', function () {
-    gulp.src('less/big-screen.less')
+    gulp.src('../less/big-screen.less')
         .pipe(less())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('../css'));
+});
+
+
+gulp.task('testWatch', function () {
+    gulp.watch('../less/**/*.less', ['testLess']); //当所有less文件发生改变时，调用testLess任务
 });
