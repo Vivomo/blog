8288638721c849513f9b9cd61e6029fb24e3321c
Vimo <929992114@qq.com>
2017-06-24 17:34:32 +0800

    industry start

--- js/big-screen.js
+++ js/big-screen.js
@@ -4,5 +4,22 @@
         const div = document.createElement('div');
         div.innerHTML = '<i class="coner c1"></i><i class="coner c2"></i><i class="coner c3"></i><i class="coner c4"></i>';
         modal.appendChild(div);
-    })
+    });
+
+    function drawIndustry(data) {
+        let max = 0,
+            dataLength = data.length,
+            splitAngle = Math.PI / dataLength;
+        data.forEach((item) => {
+            if (max < item.value) {
+                max = item.value;
+            }
+        });
+
+        
+
+
+    }
+    
+
 })();
