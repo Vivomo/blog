a2ae2bddea03f4f81e6b5ed94a0b52de567f61a3
Vimo <929992114@qq.com>
2017-06-27 13:56:30 +0800

    big screen 人才需求 radar

--- js/big-screen.js
+++ js/big-screen.js
@@ -140,6 +140,249 @@
      */
     function drawTalentsDemand(data1, data2) {
         drawLine(data1);
+        drawDemandFever(data2);
+    }
+
+    /**
+     *  雷达
+     * @param elem
+     * @param d
+     * @param options
+     */
+    function drawRadar(elem, d, options) {
+        var cfg = {
+            radius: 5,
+            w: 600,
+            h: 600,
+            factor: 1,
+            factorLegend: .85,
+            levels: 3,
+            maxValue: 0,
+            radians: 2 * Math.PI,
+            opacityArea: 0.5,
+            ToRight: 5,
+            TranslateX: 80,
+            TranslateY: 30,
+            ExtraWidthX: 100,
+            ExtraWidthY: 100,
+            color: function () {
+                return '#70c7db';
+            }
+        };
+
+        // 设置最大值
+        var maxValue = 0;
+        d.forEach((function (arr) {
+            arr.forEach(function (item) {
+                if (item.value > maxValue) {
+                    maxValue = item.value;
+                }
+            })
+        }));
+        cfg.maxValue = Math.floor(maxValue / cfg.levels) * (cfg.levels + 0.5);
+
+        // 配置合并
+        if (options) {
+            Object.entries(options).forEach(function (entry) {
+                if (entry[1]) {
+                    cfg[entry[0]] = entry[1];
+                }
+            });
+        }
+
+
+        var allAxis = (d[0].map(function (i) {
+            return i.name
+        }));
+
+        var total = allAxis.length;
+        var radius = cfg.factor * Math.min(cfg.w / 2, cfg.h / 2);
+        d3.select(elem).select("svg").remove();
+
+        var g = d3.select(elem)
+            .append("svg")
+            .attr("width", cfg.w + cfg.ExtraWidthX)
+            .attr("height", cfg.h + cfg.ExtraWidthY)
+            .append("g")
+            .attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
+
+        //Circular segments
+        for (var j = 0; j < cfg.levels; j++) {
+            var levelFactor = cfg.factor * radius * ((j + 1) / cfg.levels);
+            g.selectAll(".levels")
+                .data(allAxis)
+                .enter()
+                .append("svg:line")
+                .attr("x1", function (d, i) {
+                    return levelFactor * (1 - cfg.factor * Math.sin(i * cfg.radians / total));
+                })
+                .attr("y1", function (d, i) {
+                    return levelFactor * (1 - cfg.factor * Math.cos(i * cfg.radians / total));
+                })
+                .attr("x2", function (d, i) {
+                    return levelFactor * (1 - cfg.factor * Math.sin((i + 1) * cfg.radians / total));
+                })
+                .attr("y2", function (d, i) {
+                    return levelFactor * (1 - cfg.factor * Math.cos((i + 1) * cfg.radians / total));
+                })
+                .attr("class", "line")
+                .style("stroke", "grey")
+                .style("stroke-opacity", "0.75")
+                .style("stroke-width", "0.3px")
+                .attr("transform", "translate(" + (cfg.w / 2 - levelFactor) + ", " + (cfg.h / 2 - levelFactor) + ")");
+        }
+
+        //Text indicating at what % each level is
+//            for (var j = 0; j < cfg.levels; j++) {
+//                var levelFactor = cfg.factor * radius * ((j + 1) / cfg.levels);
+//                g.selectAll(".levels")
+//                    .data([1]) //dummy data
+//                    .enter()
+//                    .append("svg:text")
+//                    .attr("x", function (d) {
+//                        return levelFactor * (1 - cfg.factor * Math.sin(0));
+//                    })
+//                    .attr("y", function (d) {
+//                        return levelFactor * (1 - cfg.factor * Math.cos(0));
+//                    })
+//                    .attr("class", "legend")
+//                    .style("font-family", "sans-serif")
+//                    .style("font-size", "10px")
+//                    .attr("transform", "translate(" + (cfg.w / 2 - levelFactor + cfg.ToRight) + ", " + (cfg.h / 2 - levelFactor) + ")")
+//                    .attr("fill", "#737373")
+//                    .text((j + 1) * 100 / cfg.levels);
+//            }
+
+        var series = 0;
+
+        var axis = g.selectAll(".axis")
+            .data(allAxis)
+            .enter()
+            .append("g")
+            .attr("class", "axis");
+
+        axis.append("line")
+            .attr("x1", cfg.w / 2)
+            .attr("y1", cfg.h / 2)
+            .attr("x2", function (d, i) {
+                return cfg.w / 2 * (1 - cfg.factor * Math.sin(i * cfg.radians / total));
+            })
+            .attr("y2", function (d, i) {
+                return cfg.h / 2 * (1 - cfg.factor * Math.cos(i * cfg.radians / total));
+            })
+            .attr("class", "line")
+            .style("stroke", "grey")
+            .style("stroke-width", "1px");
+
+        axis.append("text")
+            .attr("class", "legend")
+            .text(function (d) {
+                return d
+            })
+            .style("font-family", "Microsoft Yahei")
+            .style("font-size", "12px")
+            .attr("text-anchor", "middle")
+            .attr("dy", "1.5em")
+            .attr('fill', '#6cc6da')
+            .attr("transform", function (d, i) {
+                return "translate(0, -10)"
+            })
+            .attr("x", function (d, i) {
+                return cfg.w / 2 * (1 - cfg.factorLegend * Math.sin(i * cfg.radians / total)) - 60 * Math.sin(i * cfg.radians / total);
+            })
+            .attr("y", function (d, i) {
+                return cfg.h / 2 * (1 - Math.cos(i * cfg.radians / total)) - 20 * Math.cos(i * cfg.radians / total);
+            });
+
+
+        var dataValues = [];
+        d.forEach(function (y, x) {
+//                var dataValues = [];
+            g.selectAll(".nodes")
+                .data(y, function (j, i) {
+                    dataValues.push([
+                        cfg.w / 2 * (1 - (parseFloat(Math.max(j.value, 0)) / cfg.maxValue) * cfg.factor * Math.sin(i * cfg.radians / total)),
+                        cfg.h / 2 * (1 - (parseFloat(Math.max(j.value, 0)) / cfg.maxValue) * cfg.factor * Math.cos(i * cfg.radians / total))
+                    ]);
+                });
+            dataValues.push(dataValues[0]);
+            g.selectAll(".name")
+                .data([dataValues])
+                .enter()
+                .append("polygon")
+                .attr("class", "radar-chart-serie" + series)
+                .style("stroke-width", "2px")
+                .style("stroke", cfg.color(series))
+                .attr("points", function (d) {
+                    var str = "";
+                    for (var pti = 0; pti < d.length; pti++) {
+                        str = str + d[pti][0] + "," + d[pti][1] + " ";
+                    }
+                    return str;
+                })
+                .style("fill", function (j, i) {
+                    return cfg.color(series)
+                })
+                .style("fill-opacity", cfg.opacityArea)
+                .on('mouseover', function (d) {
+                    var z = "polygon." + d3.select(this).attr("class");
+                    g.selectAll("polygon")
+                        .transition(200)
+                        .style("fill-opacity", 0.1);
+                    g.selectAll(z)
+                        .transition(200)
+                        .style("fill-opacity", .7);
+                })
+                .on('mouseout', function () {
+                    g.selectAll("polygon")
+                        .transition(200)
+                        .style("fill-opacity", cfg.opacityArea);
+                });
+            series++;
+        });
+        series = 0;
+
+
+        var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+        d.forEach(function (y, x) {
+            g.selectAll(".nodes")
+                .data(y).enter()
+                .append("svg:circle")
+                .attr("class", "radar-chart-serie" + series)
+                .attr('r', cfg.radius)
+                .attr("alt", function (j) {
+                    return Math.max(j.value, 0)
+                })
+                .attr("cx", function (j, i) {
+                    dataValues.push([
+                        cfg.w / 2 * (1 - (parseFloat(Math.max(j.value, 0)) / cfg.maxValue) * cfg.factor * Math.sin(i * cfg.radians / total)),
+                        cfg.h / 2 * (1 - (parseFloat(Math.max(j.value, 0)) / cfg.maxValue) * cfg.factor * Math.cos(i * cfg.radians / total))
+                    ]);
+                    return cfg.w / 2 * (1 - (Math.max(j.value, 0) / cfg.maxValue) * cfg.factor * Math.sin(i * cfg.radians / total));
+                })
+                .attr("cy", function (j, i) {
+                    return cfg.h / 2 * (1 - (Math.max(j.value, 0) / cfg.maxValue) * cfg.factor * Math.cos(i * cfg.radians / total));
+                })
+                .attr("data-id", function (j) {
+                    return j.name
+                })
+                .style("fill", "#fff")
+                .style("stroke-width", "2px")
+                .style("stroke", cfg.color(series)).style("fill-opacity", .9)
+                .on('mouseover', function (d) {
+                    console.log(d.name)
+                    tooltip
+                        .style("left", d3.event.pageX - 40 + "px")
+                        .style("top", d3.event.pageY - 80 + "px")
+                        .style("display", "inline-block")
+                        .html((d.name) + "<br><span>" + (d.value) + "</span>");
+                })
+                .on("mouseout", function (d) {
+                    tooltip.style("display", "none");
+                });
+
+            series++;
+        });
     }
 
     /**
@@ -228,6 +471,17 @@
 
     }
 
+    function drawDemandFever(data) {
+        var config = {
+            w: 150,
+            h: 150,
+            levels: 3,
+            ExtraWidthX: 300
+        };
+
+        drawRadar(document.getElementById('radar-content'), data, config);
+    }
+
     const industryData = [
         {name: '企业服务', value: 92},
         {name: '互联网', value: 57},
@@ -246,12 +500,23 @@
         [2017, 111, 145, 240]
     ];
 
+    var DemandFeverData = [
+        [
+            {"name": "Central ", "value": 80},
+            {"name": "Kirkdale", "value": 40},
+            {"name": "Kensington ", "value": 40},
+            {"name": "Everton ", "value": 90},
+            {"name": "Picton ", "value": 60},
+            {"name": "Riverside ", "value": 80}
+        ]
+    ];
+
 
     // drawIndustry(industryData);
 
     // drawDistribution(industryData);
 
-    drawTalentsDemand(talensDemand);
+    drawTalentsDemand(talensDemand, DemandFeverData);
 
 
 })();
