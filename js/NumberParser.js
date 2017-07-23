12d59bec2c91152d8aecb7b18790f4dbc651bbf6
Vimo <929992114@qq.com>
2017-07-24 02:00:37 +0800

    Number Parser finish

--- js/NumberParser.js
+++ js/NumberParser.js
@@ -1,7 +1,56 @@
 /**
- * 格式化狮子  四千八百六十九 => 4869
+ * 格式化数字  四千八百六十九 => 4869
  * @constructor
  */
-function NumberParse() {
+var NumberParser = (function () {
 
-}
+    var numberMap = {
+        一: 1,
+        二: 2,
+        三: 3,
+        四: 4,
+        五: 5,
+        六: 6,
+        七: 7,
+        八: 8,
+        九: 9
+    };
+
+    var quantifierMap = {
+        十: 10,
+        百: 100,
+        千: 1000,
+        万: 10000
+    };
+
+    var zeroToken = '零';
+
+    function translate(num){
+        var tokens = [];
+        for (var i = 0; i < num.length; i++) {
+            var char = num[i];
+            if (char in numberMap) {
+                char.push([numberMap[char], 1]);
+            } else if (char in quantifierMap) {
+                var lastToken = tokens[tokens.length - 1];
+                lastToken[1] *= quantifierMap[char];
+            } else {
+                if (zeroToken !== char) {
+                    throw '不合法的数字'
+                }
+            }
+        }
+        return tokens;
+    }
+
+    return {
+        parse: function (num) {
+            var tokens = translate(num);
+            var sum = 0;
+            tokens.forEach(function (token) {
+                sum += token[0] * token[1];
+            });
+            return sum;
+        }
+    }
+})();
