62068bf00f4c76853b5623fd9d9e23f1a1942781
Vimo <929992114@qq.com>
2017-07-27 15:48:33 +0800

    ps parse Img

--- js/ps.js
+++ js/ps.js
@@ -4,6 +4,7 @@ var Canvas = (function () {
     var loadFile = document.getElementById('loadFile'),
         canvas = document.getElementById('canvas'),
         webImg = document.getElementById('webImg'),
+        pasteImg = document.getElementById('pasteImg'),
         loadWebImg = document.getElementById('loadWebImg');
     return {
         loadFile,
@@ -19,6 +20,16 @@ var Canvas = (function () {
                 pen.drawImage(this, 0, 0);
             };
         },
+        imgReader: function( item ){
+            var blob = item.getAsFile(),
+                reader = new FileReader();
+            // 读取文件后将其显示在网页中
+            reader.onload = (e) => {
+                this.drawImgOnCanvas(e.target.result);
+            };
+            // 读取文件
+            reader.readAsDataURL( blob );
+        },
         init: function () {
             this.loadFile.onchange = () => {
                 this.drawImgOnCanvas(URL.createObjectURL(this.loadFile.files[0]))
@@ -26,6 +37,33 @@ var Canvas = (function () {
 
             loadWebImg.addEventListener('click', () => {
                 this.drawImgOnCanvas(webImg.value)
+            });
+
+            pasteImg.addEventListener( 'paste', (e) => {
+                // 添加到事件对象中的访问系统剪贴板的接口
+                var clipboardData = e.clipboardData,
+                    items, item, types;
+
+                if( clipboardData ){
+                    console.log(clipboardData);
+                    items = clipboardData.items;
+                    if( !items ){
+                        return;
+                    }
+                    item = items[0];
+                    // 保存在剪贴板中的数据类型
+                    types = clipboardData.types || [];
+                    for(var i = 0; i < types.length; i++ ){
+                        if( types[i] === 'Files' ){
+                            item = items[i];
+                            break;
+                        }
+                    }
+                    // 判断是否为图片数据
+                    if( item && item.kind === 'file' && item.type.match(/^image\//i) ){
+                        this.imgReader( item );
+                    }
+                }
             });
         }
     }
