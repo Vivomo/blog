69ef9d5c7f6635362136aec0d188de80a43092d5
Vimo <929992114@qq.com>
2017-08-07 15:30:52 +0800

    add scss to gulp

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,15 +1,30 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    scss = require('gulp-sass'),
+    scssSrc = '../scss/**/*.scss';
+    // scssSrc = '../scss/calendar.scss';
 
 gulp.task('testLess', function () {
     gulp.src('../less/questionnaire-survey.less')
         .pipe(less())
         .pipe(gulp.dest('../css'));
 });
+/**
+ * 安装scss 报错参照此网址, 推荐方法2
+ * https://github.com/lmk123/blog/issues/28
+ */
+gulp.task('scss', function () {
+    gulp.src(scssSrc)
+        .pipe(scss({
+            // outputStyle: 'expanded',
+            // indentWidth: 4
+        }))
+        .pipe(gulp.dest('../css'));
+});
 
 
 gulp.task('testWatch', function () {
-    gulp.watch('../less/**/questionnaire-survey.less', ['testLess']); //当所有less文件发生改变时，调用testLess任务
+    gulp.watch(scssSrc, ['scss']);
 });
 
-gulp.task('default', ['testLess', 'testWatch']);
+gulp.task('default', ['scss', 'testWatch']);
