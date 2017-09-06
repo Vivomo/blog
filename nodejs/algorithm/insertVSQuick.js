8c7f711e1e9dbd8bada8d051b2b9c5b367b3a622
Vimo <929992114@qq.com>
2017-09-06 20:16:16 +0800

    二分搜索插入排序

--- nodejs/algorithm/insertVSQuick.js
+++ nodejs/algorithm/insertVSQuick.js
@@ -5,7 +5,7 @@
 
 
 /**
- * 二分搜索插入排序
+ * 插入排序
  * @param arr
  */
 function insertSort(arr){
@@ -22,6 +22,46 @@ function insertSort(arr){
     }
 }
 
+/**
+ * 二分搜索插入排序
+ * @param arr
+ */
+function binarySearchInsertSort(arr) {
+    for (let i = 1, l = arr.length; i < l; i++) {
+        let begin = 0,
+            end = i - 1,
+            mid, target;
+
+        while (end - begin > 1) {
+            mid = ~~ ((begin + end) / 2);
+            if (arr[i] < arr[mid]) {
+                end = mid - 1;
+            } else if (arr[i] > arr[mid]) {
+                begin = mid + 1;
+            } else {
+                target = mid + 1;
+                break;
+            }
+        }
+        if (!target) {
+            if (arr[i] < arr[begin]) {
+                target = begin;
+            } else if (arr[i] < arr[end]) {
+                target = end;
+            } else {
+                target = end + 1;
+            }
+        }
+
+        for (let j = i; j > target; j--) {
+            const temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+        }
+        console.log('->', arr)
+    }
+}
+
 function getRandomArray(len, max=100000) {
     var arr = [];
     for (let i = 0; i < len; i++) {
@@ -29,9 +69,10 @@ function getRandomArray(len, max=100000) {
     }
     return arr;
 }
-
-var arr = getRandomArray(20);
+//  [ 92, 46, 86, 85, 77, 7, 51, 6, 36, 40, 25, 68, 26, 28, 29, 64, 6, 72, 37, 61 ]
+var arr = getRandomArray(30, 1000);
 
 console.log('before sort', arr);
-insertSort(arr);
-console.log('after sort', arr);
+binarySearchInsertSort(arr);
+console.log('after sort', arr.toString());
+
