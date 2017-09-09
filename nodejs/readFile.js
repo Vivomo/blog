7ba194f90c45e977dca42e7a8a3833a3e0ce2b42
Vimo <929992114@qq.com>
2017-09-10 00:13:27 +0800

    读取单词 过滤标签

--- nodejs/readFile.js
+++ nodejs/readFile.js
@@ -1,26 +1,37 @@
 const fs = require('fs');
 
+const contentReg = />([^<]+)</g;
 const wordReg = /[a-zA-Z]\w+/g;
 
-fs.readFile('F:\\doc\\es6.html', function (err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    const content = data.toString();
-    // const content = 'abc aaa ddd';
-    const dictionary = {};
-
-    let result;
-
-    while (result = wordReg.exec(content)) {
-        const word = result[0].toLowerCase();
-        if (dictionary[word]) {
-            dictionary[word] += 1;
-        } else {
-            dictionary[word] = 1;
+function readFile() {
+    fs.readFile('F:\\doc\\es6.html', function (err, data) {
+        if (err) {
+            return console.error(err);
         }
-    }
-    console.log(dictionary);
-    console.log('单词个数', Object.keys(dictionary).length);
-    console.log('最常用的单词排序', Object.entries(dictionary).sort((a, b) => b[1] - a[1]).slice(0, 100));
-});
+        const content = data.toString();
+        // const content = 'abc aaa ddd';
+        const dictionary = {};
+
+        let result;
+
+        while (result = contentReg.exec(content)) {
+            const content = result[1].toLowerCase();
+            const words = content.match(wordReg);
+            if (words) {
+                words.forEach((word) => {
+                    if (dictionary[word]) {
+                        dictionary[word] += 1;
+                    } else {
+                        dictionary[word] = 1;
+                    }
+                })
+            }
+        }
+        console.log(dictionary);
+        console.log('单词个数', Object.keys(dictionary).length);
+        console.log('最常用的单词排序', Object.entries(dictionary).sort((a, b) => b[1] - a[1]).slice(0, 100));
+    });
+}
+
+
+readFile();
