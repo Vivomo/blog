806310713e8f085a14c482ff2abc5db3114603f1
Vimo <929992114@qq.com>
2017-09-10 23:15:03 +0800

    重构代码结构

--- nodejs/countES6DocWord.js
+++ nodejs/countES6DocWord.js
@@ -3,38 +3,51 @@ const fs = require('fs');
 const contentReg = />([^<]+)</g; // JS 不支持 (?<=) 所以只有 (?=)没用
 const wordReg = /[a-zA-Z]\w+/g;
 
-function countWordByReg() {
-    fs.readFile('F:\\doc\\es6.html', function (err, data) {
-        if (err) {
-            return console.error(err);
-        }
-        const content = data.toString();
-        const dictionary = {};
+fs.readFile('F:\\doc\\es6.html', function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    const html = data.toString();
+    countWordByReg(html);
+});
 
-        let result;
+/**
+ * 使用正则来统计单词
+ * @param html
+ */
+function countWordByReg(html) {
+    const dictionary = {};
 
-        while (result = contentReg.exec(content)) {
-            const content = result[1].toLowerCase();
-            const words = content.match(wordReg);
-            if (words) {
-                words.forEach((word) => {
-                    if (dictionary[word]) {
-                        dictionary[word] += 1;
-                    } else {
-                        dictionary[word] = 1;
-                    }
-                })
-            }
+    let result;
+
+    while (result = contentReg.exec(html)) {
+        const content = result[1].toLowerCase();
+        const words = content.match(wordReg);
+        if (words) {
+            words.forEach((word) => {
+                if (dictionary[word]) {
+                    dictionary[word] += 1;
+                } else {
+                    dictionary[word] = 1;
+                }
+            })
         }
-        printDictionary(dictionary)
-    });
+    }
+    printDictionary(dictionary)
 }
 
+
+
+function countWordByFiniteState(html) {
+
+}
+
+/**
+ * 打印统计结果
+ * @param dictionary
+ */
 function printDictionary(dictionary) {
     console.log(dictionary);
     console.log('单词个数', Object.keys(dictionary).length);
     console.log('最常用的单词排序', Object.entries(dictionary).sort((a, b) => b[1] - a[1]).slice(0, 100));
 }
-
-
-countWordByReg();
