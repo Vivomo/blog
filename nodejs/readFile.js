a961ef6fe1583f36433818c10855c1efa7765819
Vimo <929992114@qq.com>
2017-09-10 23:08:48 +0800

    rename function name

--- nodejs/readFile.js
+++ nodejs/readFile.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-const contentReg = />([^<]+)</g;
+const contentReg = />([^<]+)</g; // JS 不支持 (?<=) 所以只有 (?=)没用
 const wordReg = /[a-zA-Z]\w+/g;
 
-function readFile() {
+function countWordByReg() {
     fs.readFile('F:\\doc\\es6.html', function (err, data) {
         if (err) {
             return console.error(err);
         }
         const content = data.toString();
-        // const content = 'abc aaa ddd';
         const dictionary = {};
 
         let result;
@@ -27,11 +26,15 @@ function readFile() {
                 })
             }
         }
-        console.log(dictionary);
-        console.log('单词个数', Object.keys(dictionary).length);
-        console.log('最常用的单词排序', Object.entries(dictionary).sort((a, b) => b[1] - a[1]).slice(0, 100));
+        printDictionary(dictionary)
     });
 }
 
+function printDictionary(dictionary) {
+    console.log(dictionary);
+    console.log('单词个数', Object.keys(dictionary).length);
+    console.log('最常用的单词排序', Object.entries(dictionary).sort((a, b) => b[1] - a[1]).slice(0, 100));
+}
 
-readFile();
+
+countWordByReg();
