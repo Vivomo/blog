012c22296de783de5daa2db521915a6cf3f43fd4
Vimo <929992114@qq.com>
2017-09-11 10:01:09 +0800

    八进制

--- nodejs/algorithm/hex.js
+++ nodejs/algorithm/hex.js
@@ -14,5 +14,18 @@ function toBinary(num){
     return arr.join('');
 }
 
+/**
+ * 八进制
+ * @param num
+ */
+function toOctal(num) {
+    let arr = [];
+    do {
+        arr.unshift(num & 7);
+    } while (num >>= 3);
+    return arr.join('');
+}
+
 
 console.log(toBinary(80));
+console.log(toOctal(100) === (100).toString(8));
