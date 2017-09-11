067db085c785954f0cf0ccd40aa2d8d5d5b9017f
Vimo <929992114@qq.com>
2017-09-11 10:08:29 +0800

    十六进制

--- nodejs/algorithm/hex.js
+++ nodejs/algorithm/hex.js
@@ -2,6 +2,7 @@
  * 进制转换
  */
 
+const hexArr = '0123456789ABCDEF'.split('');
 /**
  * 二进制
  * @param num
@@ -9,7 +10,7 @@
 function toBinary(num){
     let arr = [];
     do {
-        arr.unshift(num & 1);
+        arr.unshift(hexArr[num & 1]);
     } while (num >>= 1);
     return arr.join('');
 }
@@ -21,11 +22,23 @@ function toBinary(num){
 function toOctal(num) {
     let arr = [];
     do {
-        arr.unshift(num & 7);
+        arr.unshift(hexArr[num & 7]);
     } while (num >>= 3);
     return arr.join('');
+}
+
+/**
+ * 十六进制
+ */
+function toHexadecimal(num) {
+    let arr = [];
+    do {
+        arr.unshift(hexArr[num & 15]);
+    } while (num >>= 4);
+    return arr.join('')
 }
 
 
 console.log(toBinary(80));
 console.log(toOctal(100) === (100).toString(8));
+console.log(toHexadecimal(100) === (100).toString(16));
