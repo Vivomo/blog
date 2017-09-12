a19a83953927f825b44de0a131b16e3dfd735273
Vimo <929992114@qq.com>
2017-09-12 20:28:34 +0800

    创建随机数组的方法

---
+++ nodejs/util/arrUtil.js
@@ -0,0 +1,13 @@
+/**
+ * 一些生成数组的方法
+ */
+
+export function getRandomArr(len, min = 0, max = 10000) {
+    let arr = [];
+    let diff = max - min;
+    for (let i = 0; i < len; i++) {
+        arr.push(min + ~~(Math.random() * diff))
+    }
+    return arr;
+}
+
