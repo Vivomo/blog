ed03e06c3a5e3acd5179cbd834a98bd20074cd76
Vimo <929992114@qq.com>
2017-09-12 20:21:13 +0800

    简单算法求数组最大和

---
+++ nodejs/algorithm/getMaxSumOfArr.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * 获取 数组中连续片段的最大和
+ */
+
+/**
+ * 简单算法
+ * @param arr
+ */
+function simple(arr) {
+    let max = 0;
+    let begin = 0;
+    let end = 0;
+    for (let i = 0, l = arr.length; i < l; i++ ) {
+        let sum = 0;
+        for (let j = i; i < l; j++) {
+            sum += arr[j];
+            if (sum > max) {
+                max = sum;
+                begin = i;
+                end = j;
+            }
+        }
+    }
+    console.log(begin, end, max);
+    return max;
+}
