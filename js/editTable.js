b6a4660ad56877d7b06dc9d080fa5fd096ba476c
Vimo <929992114@qq.com>
2017-09-18 14:57:38 +0800

    no message

--- js/editTable.js
+++ js/editTable.js
@@ -8,6 +8,7 @@ class Table {
     init() {
         const {col, row} = this;
         const table = this.table = document.createElement('table');
+        table.className = 'edit-table';
         for (let i = 0; i <= row; i++) {
             const tr = document.createElement('tr');
             table.appendChild(tr);
@@ -19,9 +20,10 @@ class Table {
             }
         }
         this._initColRowIndex();
+        this._initWrapElem();
         this._initTable();
         this._initTableSelect();
-        this._initCoordinate();
+        this._updateCellsPosition();
     }
 
 
@@ -29,18 +31,39 @@ class Table {
         this.getCell(col, row).innerText = text;
     }
 
+    /**
+     * 获取对应坐标的TD
+     * @param col
+     * @param row
+     * @returns {HTMLElement}
+     */
     getCell(col, row) {
         return this.trs[row].children[col];
     }
 
     /**
+     * 获取一个单元格的位置
+     * @param cell
+     * @private
+     */
+    _getCellPosition(cell) {
+        const cellBCR = cell.getBoundingClientRect();
+        return {
+            top: cell.offsetTop,
+            bottom: this.wrapBCR.bottom - cellBCR.bottom,
+            left: cell.offsetLeft,
+            right: this.wrapBCR.right - cellBCR.right,
+        }
+    }
+
+    /**
      * 合并单元格
-     * @param from  [col, row]
-     * @param to    [col, row]
+     * @param from  {col, row}
+     * @param to    {col, row}
      */
     mergeCell(from, to) {
-        const [startCol, endCol] = [from[0], to[0]].sort();
-        const [startRow, endRow] = [from[1], to[1]].sort();
+        const [startCol, endCol] = [from.col, to.col].sort();
+        const [startRow, endRow] = [from.row, to.row].sort();
         const startTd = this.getCell(startCol, startRow);
 
         const colSpanCount = endCol - startCol + 1;
@@ -70,9 +93,45 @@ class Table {
         startTd.rowSpan = rowSpanCount;
 
         startTd.innerHTML = tdContent;
-        setTimeout(function () {
-            removeTdList.forEach(item => item.remove());
+
+        setTimeout(() => {
+            const startTdPosition = this._getCellPosition(startTd);
+            removeTdList.forEach(item => {
+                const {col, row} = item.dataset;
+                this.cellsPosition[row][col] = startTdPosition;
+                item.remove();
+            });
         },1);
+
+    }
+
+    /**
+     * 选择区域
+     */
+    select(startPoint, endPoint = startPoint) {
+
+        const [startCol, endCol] = [startPoint.col, endPoint.col].sort();
+        const [startRow, endRow] = [startPoint.row, endPoint.row].sort();
+
+        const leftTopTdCoord = this.cellsPosition[startRow][startCol];
+        let {top, bottom, left, right} = leftTopTdCoord;
+
+        for (let i = endCol; i >= startCol; i--) {
+            top = Math.min(this.cellsPosition[startRow][i].top, top);
+            bottom = Math.min(this.cellsPosition[endRow][i].bottom, bottom);
+        }
+
+        for (let i = endRow; i >= startRow; i--) {
+            left = Math.min(this.cellsPosition[i][startCol].left, left);
+            right = Math.min(this.cellsPosition[i][endCol].right, right);
+        }
+
+        this._setTableSelect({
+            top,
+            bottom,
+            left,
+            right
+        });
 
     }
 
@@ -80,20 +139,46 @@ class Table {
      * 初始化单元格坐标
      * @private
      */
-    _initCoordinate() {
-        this.coord = new Array(this.row).fill(null).map((item, index) => {
-            return Array.from(this.trs[index].children).map(elem => elem.getBoundingClientRect().toJSON())
+    _updateCellsPosition() {
+        this.cellsPosition = new Array(this.row + 1).fill(null).map((item, index) => {
+            return Array.from(this.trs[index].children).map(elem => this._getCellPosition(elem));
         });
-        console.log(this.coord);
+    }
+
+    /**
+     * 获取一个坐标对应单元格的行列
+     * @param point
+     * @private
+     */
+    _getCoordIndex(point) {
 
     }
 
+    /**
+     * 初始化选择框
+     * @private
+     */
     _initTableSelect() {
         const tableSelect = this.tableSelect = document.createElement('div');
         tableSelect.className = 'table-select';
         this.wrapElem.appendChild(tableSelect);
     }
 
+    /**
+     * init table select
+     * @param style
+     * @private
+     */
+    _setTableSelect(style){
+        Object.entries(style).forEach(([key, value]) => {
+            this.tableSelect.style[key] = `${value}px`;
+        });
+    }
+
+    /**
+     * init 行列标注
+     * @private
+     */
     _initColRowIndex() {
         const trColIndex = this.table.firstElementChild;
         trColIndex.classList.add('tr-col-index');
@@ -113,14 +198,25 @@ class Table {
         const firstTd = trColIndex.firstElementChild;
         firstTd.classList.add('td-row-index');
     }
+
+    /**
+     * init wrap
+     * @private
+     */
+    _initWrapElem() {
+        this.wrapElem.appendChild(this.table);
+        this.wrapElem.style.width = `${41+this.col * 60}px`;
+        this.wrapBCR = this.wrapElem.getBoundingClientRect().toJSON();
+        // console.log()
+    }
     /**
      * 给table 绑定一些事件
      */
     _initTable() {
         const table = this.table;
-        this.wrapElem.appendChild(table);
         this.trs = table.children;
-        this.table.classList.add('edit-table');
+        // this.table.classList.add('edit-table');
+
 
         // 双击编辑
         this.table.addEventListener('dblclick', function (e) {
@@ -131,6 +227,7 @@ class Table {
                 const tdHeight = target.clientHeight;
 
                 target.innerHTML = `<textarea style="width: ${tdWidth}px; height: ${tdHeight}px">${text}</textarea>`;
+                console.log(target.firstElementChild);
                 const input = target.querySelector('textarea');
                 input.focus();
                 input.onblur = function () {
@@ -139,17 +236,43 @@ class Table {
             }
         });
 
-        this.table.addEventListener('mousedown', function (e) {
+        this.table.addEventListener('mousedown', (e) => {
             const target = e.target;
             if (target.tagName === 'TD') {
-                console.log(e);
+                this._updateCellsPosition();
+
+                const data = target.dataset;
+                const startPoint = {
+                    col: ~~data.col,
+                    row: ~~data.row
+                };
+                this.select(startPoint);
+
+                this.table.onmousemove = (e) => {
+                    const target = e.target;
+                    if (target.tagName === 'TD') {
+                        const data = target.dataset;
+                        const endPoint = {
+                            col: ~~data.col,
+                            row: ~~data.row
+                        };
+                        this.select(startPoint, endPoint);
+                    }
+                }
+
             }
+        });
+
+        this.table.addEventListener('mouseup', () => {
+            this.table.onmousemove = null;
         });
     }
 
+
+
 }
 
-let table = new Table('#table-wrap', 15, 30);
+let table = new Table('#table-wrap', 15, 10);
 
-table.setText(123, 3, 3);
-table.setText(1232344, 4, 9);
+// table.setText(123, 3, 3);
+// table.setText(1232344, 4, 9);
