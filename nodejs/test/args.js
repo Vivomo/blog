3c67398e6244f30f73510fc0a5cf0d199181174a
Vimo <929992114@qq.com>
2017-11-03 14:30:44 +0800

    args test

--- nodejs/test/args.js
+++ nodejs/test/args.js
@@ -1,5 +1,6 @@
 function logArgs(...args) {
     console.log(args)
+    console.log(args.constructor)
 }
 
 function test(...args) {
@@ -7,3 +8,7 @@ function test(...args) {
 }
 
 test(1, 2)
+let a = {'0':1, '1':2, length: 2}
+let b = [1, 2, 3];
+console.log(...a) // error
+console.log(...b)
