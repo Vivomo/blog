3fe67818312524cfda084b1919be2f6ba6c3bff9
Vimo <929992114@qq.com>
2018-01-17 17:24:33 +0800

    优化 qrcode 插件, 移除jQuery依赖, 增加download 功能

--- chrome_plugins/qr_code/popup.js
+++ chrome_plugins/qr_code/popup.js
@@ -1,10 +1,47 @@
 chrome.tabs.query({
     active: true
 }, function (tabs) {
-    var tab = tabs[0];
-    $('#qr-code').qrcode({
-        width: 200,
-        height: 200,
-        text: tab.url
+    var tab = tabs[0],
+        width = 200,
+        height = 200,
+        background = "#ffffff",
+        foreground = "#000000";
+
+    var qrcode	= new QRCode(-1, QRErrorCorrectLevel.H);
+    qrcode.addData(tab.url);
+    qrcode.make();
+
+    // create canvas element
+    var canvas	= document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    var ctx	= canvas.getContext('2d');
+
+    // compute tileW/tileH based on options.width/options.height
+    var tileW	= width  / qrcode.getModuleCount();
+    var tileH	= height / qrcode.getModuleCount();
+
+    // draw in the canvas
+    for( var row = 0; row < qrcode.getModuleCount(); row++ ){
+        for( var col = 0; col < qrcode.getModuleCount(); col++ ){
+            ctx.fillStyle = qrcode.isDark(row, col) ? foreground : background;
+            var w = (Math.ceil((col+1)*tileW) - Math.floor(col*tileW));
+            var h = (Math.ceil((row+1)*tileW) - Math.floor(row*tileW));
+            ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);
+        }
+    }
+
+    var wrap = document.getElementById('qr-code');
+    wrap.appendChild(canvas);
+
+    document.getElementById('download').addEventListener('click', function () {
+        var canvas = wrap.querySelector('canvas');
+        canvas.toBlob((blob) => {
+            var a = document.createElement('a');
+            a.href = window.URL.createObjectURL(blob);
+            a.download = 'download.png';
+            a.click();
+        }, 'image/png');
     });
+
 });
