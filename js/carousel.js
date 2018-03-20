3dc9df018fcffc250da32d819ea121a8e92032c0
Vimo <929992114@qq.com>
2018-03-20 15:18:35 +0800

    config 赋值bug fix touchable 优化

--- js/carousel.js
+++ js/carousel.js
@@ -5,10 +5,11 @@ class Carousel {
             transitionTime: 300,
             looped: true,
             auto: true,
-            touchable: false
+            touchable: false,
+            margin: 0
         };
 
-        for (let key of this.config) {
+        for (let key in this.config) {
             if (key in opt) {
                 this.config[key] = opt[key];
             }
@@ -19,6 +20,7 @@ class Carousel {
 
     init(opt) {
         this.transitionStyleName = 'transition' in document.body.style ? 'transition' : 'webKitTransition';
+        this.transformStyleName = 'transform' in document.body.style ? 'transform' : 'webKitTransform';
 
         let wrap = typeof opt.elem === 'string' ? document.querySelector(opt.elem) : opt.elem;
         this.wrap = wrap;
@@ -28,8 +30,11 @@ class Carousel {
         this.elem = Array.from(wrap.children);
         this.activeIndex = 0;
         this.prevActiveIndex = this.elem.length - 1;
-        this.updatePosition();
+        if (this.config.touchable) {
+            this.listenTouch();
+        }
         if (this.config.auto) {
+            this.updatePosition();
             this.start();
 
             wrap.addEventListener('mouseenter', () => {
@@ -46,21 +51,21 @@ class Carousel {
      * 更新位置
      */
     updatePosition(isNext = true) {
-        if (!this.config.looped) {
-            return false;
-        }
-        this.wrap.style[this.transitionStyleName] = 'none';
-        let showArr = [this.prevActiveIndex, this.activeIndex];
-        this.elem.filter((item, index) => !showArr.includes(index)).forEach((item) => {
-            item.style.visibility = 'hidden';
-        });
-        let nextElem = this.elem[this.activeIndex];
-        nextElem.style.visibility = 'visible';
-        this.elem[this.prevActiveIndex].style.left = this.wrap.style.left = '0px';
-        if (isNext) {
-            nextElem.style.left = this.width + 'px';
-        } else {
-            nextElem.style.left = -this.width + 'px';
+        this.toggleTransition(false);
+        if (this.config.looped) {
+            let showArr = [this.prevActiveIndex, this.activeIndex];
+            this.elem.filter((item, index) => !showArr.includes(index)).forEach((item) => {
+                item.style.visibility = 'hidden';
+            });
+            let nextElem = this.elem[this.activeIndex];
+            nextElem.style.visibility = 'visible';
+            this.elem[this.prevActiveIndex].style.left = '0px';
+            this.wrap.style[this.transformStyleName] = 'translate3d(0, 0, 0)';
+            if (isNext) {
+                nextElem.style.left = this.width + 'px';
+            } else {
+                nextElem.style.left = -this.width + 'px';
+            }
         }
         setTimeout(() => {
             this.move(isNext);
@@ -69,17 +74,29 @@ class Carousel {
 
     move(isNext = true) {
         this.moving = true;
-        this.wrap.style[this.transitionStyleName] = `all ${this.config.transitionTime}ms cubic-bezier(0.94,-0.01, 0.11, 0.97)`;
-        if (isNext) {
-            this.wrap.style.left = -this.width + 'px';
+        this.toggleTransition(true);
+        let moveDistance = 0;
+        if (this.config.looped) {
+            moveDistance = isNext ? - (this.width + this.config.margin) : this.width + this.config.margin;
         } else {
-            this.wrap.style.left = this.width + 'px';
+            moveDistance = (this.width + this.config.margin) * this.activeIndex;
         }
+        this.wrap.style[this.transformStyleName] = `translate3d(${moveDistance}px, 0, 0)`;
 
         setTimeout(() => {
             this.moving = false;
             this.afterSwitch(this.activeIndex, this.prevActiveIndex);
         }, this.config.transitionTime);
+    }
+
+    toggleTransition(isAdd) {
+        if (isAdd || this.wrap.style[this.transitionStyleName] === '' ||
+            this.wrap.style[this.transitionStyleName] === 'none') {
+            this.wrap.style[this.transitionStyleName] =
+                `all ${this.config.transitionTime}ms cubic-bezier(0.94,-0.01, 0.11, 0.97)`;
+        } else {
+            this.wrap.style[this.transitionStyleName] = 'none';
+        }
     }
 
     /**
@@ -89,7 +106,15 @@ class Carousel {
         let startX = 0;
         let startY = 0;
         let movedX = 0;
+        let touchMove = (e) => {
+            let point = e.touches[0];
+            movedX = point.pageX - startX;
+            this.wrap.style[this.transformStyleName] = `translate3d(${movedX}px, 0, 0)`;
+        };
         this.wrap.addEventListener('touchstart', (e) => {
+            if (this.moving) {
+                return;
+            }
             let point = e.touches[0];
             startX = point.pageX;
             startY = point.pageY;
@@ -101,10 +126,7 @@ class Carousel {
              this.wrap.removeEventListener('touchmove', touchMove);
         });
 
-        function touchMove(e) {
-            let point = e.touches[0];
-            movedX = point.pageX - startX;
-        }
+
     }
 
     /**
