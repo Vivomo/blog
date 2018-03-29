3a8d664cdaebd2d24d467249bd6b085ba79f8ade
Vimo <929992114@qq.com>
2018-03-29 14:59:30 +0800

    letter-nav 雏形

---
+++ test/letter-nav/letterNav.js
@@ -0,0 +1,77 @@
+class LetterNav{
+    constructor(config) {
+        this.nav = config.nav;
+        this.callback = config.callback;
+        this.extent = config.extent || 4;
+        this.init();
+    }
+
+    init() {
+        this.initView();
+        this.registerListener();
+    }
+
+    /**
+     * 初始化视图
+     */
+    initView() {
+        let wrap = this.wrap = document.createElement('ul');
+        wrap.className = 'letter-nav';
+        wrap.innerHTML = this.nav.map(item => `<li class="item">${item}</li>`).join('');
+        document.body.appendChild(wrap);
+        this.navItems = Array.from(wrap.querySelectorAll('.item'));
+        let halfNavItemHeight = this.activeDistance = this.navItems[0].clientHeight / 2;
+        this.navPositionCache = this.navItems.map(item => item.getBoundingClientRect().top - halfNavItemHeight);
+    }
+
+    /**
+     * 注册事件
+     */
+    registerListener() {
+        let body = document.body;
+        let touchMove = (e) => {
+            let screenY = e.touches[0].screenY;
+            this.refreshTouchView(screenY);
+        }
+
+        this.wrap.addEventListener('touchstart', (e) => {
+            let screenY = e.touches[0].screenY;
+            this.refreshTouchView(screenY);
+            body.addEventListener('touchmove', touchMove)
+        });
+
+        body.addEventListener('touchend', (e) => {
+            body.removeEventListener('touchmove', touchMove)
+        })
+    }
+
+    refreshTouchView(screenY) {
+        let activeIndex = this.navPositionCache.findIndex(y => Math.abs(screenY - y) <= this.activeDistance);
+        if (this.activeIndex !== activeIndex) {
+            this.callback(this.nav[activeIndex]);
+            if (this.activeIndex > -1) {
+                this.navItems[this.activeIndex].classList.remove('active');
+            }
+            this.navItems[activeIndex].classList.add('active');
+            this.activeIndex = activeIndex;
+        }
+        let startIndex = Math.max(0, activeIndex - this.extent);
+        let endIndex = Math.min(activeIndex + this.extent, this.navItems.length - 1);
+        for (let i = startIndex; i <= endIndex; i++) {
+            this.updateNavItemStyle(i, screenY);
+        }
+    }
+
+    /**
+     * 更新单个 导航的样式
+     * @param index
+     * @param screenY
+     */
+    updateNavItemStyle(index, screenY) {
+        let nav = this.navItems[index];
+        let percent = Math.abs(screenY - this.navPositionCache[index]) / (this.activeDistance * 2 * this.extent);
+        nav.style.opacity = 0.75 + 0.25 * percent;
+        nav.style.transform = `translate3d(${-100 * (1 - percent)}px, 0, 0) scale(${(1-percent) * 2 + 1})`
+
+    }
+}
