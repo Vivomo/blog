89113571285a49cb01b380da77e964d14fe1e7eb
Vimo <929992114@qq.com>
2018-03-29 15:07:11 +0800

    fix bug 可视区y轴取值

--- test/letter-nav/letterNav.js
+++ test/letter-nav/letterNav.js
@@ -21,7 +21,7 @@ class LetterNav{
         document.body.appendChild(wrap);
         this.navItems = Array.from(wrap.querySelectorAll('.item'));
         let halfNavItemHeight = this.activeDistance = this.navItems[0].clientHeight / 2;
-        this.navPositionCache = this.navItems.map(item => item.getBoundingClientRect().top - halfNavItemHeight);
+        this.navPositionCache = this.navItems.map(item => item.getBoundingClientRect().top + halfNavItemHeight);
     }
 
     /**
@@ -30,13 +30,13 @@ class LetterNav{
     registerListener() {
         let body = document.body;
         let touchMove = (e) => {
-            let screenY = e.touches[0].screenY;
-            this.refreshTouchView(screenY);
+            let clientY = e.touches[0].clientY;
+            this.refreshTouchView(clientY);
         }
 
         this.wrap.addEventListener('touchstart', (e) => {
-            let screenY = e.touches[0].screenY;
-            this.refreshTouchView(screenY);
+            let clientY = e.touches[0].clientY;
+            this.refreshTouchView(clientY);
             body.addEventListener('touchmove', touchMove)
         });
 
@@ -45,8 +45,8 @@ class LetterNav{
         })
     }
 
-    refreshTouchView(screenY) {
-        let activeIndex = this.navPositionCache.findIndex(y => Math.abs(screenY - y) <= this.activeDistance);
+    refreshTouchView(clientY) {
+        let activeIndex = this.navPositionCache.findIndex(y => Math.abs(clientY - y) <= this.activeDistance);
         if (this.activeIndex !== activeIndex) {
             this.callback(this.nav[activeIndex]);
             if (this.activeIndex > -1) {
@@ -58,18 +58,18 @@ class LetterNav{
         let startIndex = Math.max(0, activeIndex - this.extent);
         let endIndex = Math.min(activeIndex + this.extent, this.navItems.length - 1);
         for (let i = startIndex; i <= endIndex; i++) {
-            this.updateNavItemStyle(i, screenY);
+            this.updateNavItemStyle(i, clientY);
         }
     }
 
     /**
      * 更新单个 导航的样式
      * @param index
-     * @param screenY
+     * @param clientY
      */
-    updateNavItemStyle(index, screenY) {
+    updateNavItemStyle(index, clientY) {
         let nav = this.navItems[index];
-        let percent = Math.abs(screenY - this.navPositionCache[index]) / (this.activeDistance * 2 * this.extent);
+        let percent = Math.abs(clientY - this.navPositionCache[index]) / (this.activeDistance * 2 * this.extent);
         nav.style.opacity = 0.75 + 0.25 * percent;
         nav.style.transform = `translate3d(${-100 * (1 - percent)}px, 0, 0) scale(${(1-percent) * 2 + 1})`
 
