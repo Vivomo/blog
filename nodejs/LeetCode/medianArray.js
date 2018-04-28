d8d22d4d949e990a6f7c516899d07c54d15933a3
Vimo <929992114@qq.com>
2018-04-28 16:38:43 +0800

    问题注释

--- nodejs/LeetCode/medianArray.js
+++ nodejs/LeetCode/medianArray.js
@@ -1,4 +1,9 @@
 /**
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+ Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ */
+/**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
@@ -23,6 +28,7 @@ let findMedianSortedArrays = function(nums1, nums2) {
         } else {
             i++;
         }
-
     }
+
+    // TODO
 };
