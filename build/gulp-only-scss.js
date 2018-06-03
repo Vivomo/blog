39d94f6ac539e8caca0cdfbce14f7ff92f2c3c53
Vimo <929992114@qq.com>
2018-06-03 23:45:33 +0800

    add scss build

---
+++ build/gulp-only-scss.js
@@ -0,0 +1,20 @@
+let gulp = require('gulp'),
+    scss = require('gulp-sass'),
+    scssSrc = '../scss/**/*.scss';
+
+gulp.task('scss', function () {
+    gulp.src(scssSrc)
+        .pipe(scss({
+            // outputStyle: 'expanded',
+            // indentWidth: 4
+        }))
+        .pipe(gulp.dest('../css'));
+});
+
+
+
+gulp.task('testWatch', function () {
+    gulp.watch(scssSrc, ['scss']);
+});
+
+gulp.task('default', ['scss', 'testWatch']);
