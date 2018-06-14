ebf2da38b82af8a664892729439a70d7ab61cc07
Vimo <929992114@qq.com>
2018-06-14 16:59:30 +0800

    八皇后问题解决

--- nodejs/algorithm/eightQueens.js
+++ nodejs/algorithm/eightQueens.js
@@ -2,44 +2,30 @@
 
 const queensNum = 8;
 const max = 63;
-let queens = [];
+let queens = [0];
 let end = 0;
-for (let i = 0; i < queensNum; i++) {
-    queens.push(i);
-    end += max - i;
-}
 
 let maxCount = 4426165368; // 64C8
 let count = 0;
-let arrSum = getArraySum(queens);
 
-while (arrSum < end) {
-    for (let queenIndex = queensNum - 1; queenIndex >= 0; queenIndex--) {
-        let queen = queens[queenIndex];
-        let siblingQueen = queens.filter(item => item !== queen);
-        for (let queenValue = queen; queenValue <= max; queenValue++) {
-            if (!siblingQueen.includes(queenIndex) && valid([...siblingQueen, queen])) {
-                siblingQueen.splice(queenIndex, 0, queenValue);
-                console.log(siblingQueen, 'result');
-            }
-            count ++;
-            if (count > maxCount) {
-                throw 'over'
-            }
+while (count < maxCount) {
+    count ++;
+    let last = queens[queens.length - 1]
+    if (valid(queens)) {
+        if (queens.length === queensNum) {
+            console.log('result', queens, count)
+            break;
+        } else {
+            queens.push(last + 1);
         }
-        if (queen < max) {
-            queens[queenIndex]++
+    } else {
+        if (last === max) {
+            queens.pop()
         }
+        queens[queens.length - 1]++;
     }
-    console.log(queens, count)
-    arrSum = getArraySum(queens);
 }
 
-console.log(count, 'count')
-
-function getArraySum(arr) {
-    return arr.reduce((a, b) => a + b, 0)
-}
 
 
 function valid(queens) {
@@ -85,9 +71,3 @@ function valid(queens) {
 let ans = [3, 14, 18, 31, 33, 44, 48, 61]
 let ans2 = [3, 14, 18, 31, 33, 44, 48, 62]
 
-console.log(valid([1]))
-console.log(valid([1, 19]))
-console.log(valid([1, 10]))
-console.log(valid([1, 11]))
-console.log(valid(ans))
-console.log(valid(ans2))
