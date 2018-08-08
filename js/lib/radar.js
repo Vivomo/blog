c2b64b67a5d05d4b7cad7d167bb8c43d6bbf51a2
Vimo <929992114@qq.com>
2018-08-09 03:44:09 +0800

    重构 radar

--- js/lib/radar.js
+++ js/lib/radar.js
@@ -1,27 +1,32 @@
 class Radar{
     constructor({canvas, data}) {
         this.canvas = document.querySelector(canvas);
-        this.ctx = this.canvas.getContext('2d');
         this.data = data;
+        this.init();
+    }
+
+    init() {
+        this.ctx = this.canvas.getContext('2d');
+        this.indicatorCount = this.data.indicator.length;
+        this.indicatorRad = Math.PI * 2 / this.indicatorCount;
+
+        let {width, height} = this.canvas;
+        let minSide = Math.min(width, height);
+        this.r = minSide * 0.85 / 2; //参照了百度雷达半径和短边的对比
     }
 
     draw() {
         let {width, height} = this.canvas;
-        let ctx = this.ctx;
-        let data = this.data;
+        let {ctx, r, indicatorCount, indicatorRad} = this;
         ctx.clearRect(0, 0, width, height);
 
-        let minSide = Math.min(width, height);
-        let r = minSide * 0.85 / 2; // 参照了百度雷达半径和短边的对比
-        let indicatorCount = data.indicator.length;
         let centerX = ~~(width / 2);
         let centerY = ~~(height / 2);
-        let rad = Math.PI * 2 / indicatorCount;
 
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#999';
         for (let i = 0; i < indicatorCount; i++) {
-            let degree = Math.PI / 2 -  rad * i;
+            let degree = Math.PI / 2 -  indicatorRad * i;
             let x = centerX + Math.cos(degree) * r;
             let y = centerX - Math.sin(degree) * r;
             ctx.moveTo(centerX, centerY);
@@ -32,7 +37,7 @@ class Radar{
         for (let j = 1; j <= 5; j++) {
             for (let i = 0; i < indicatorCount; i++) {
                 let _r = r * j / 5;
-                let degree = Math.PI / 2 -  rad * i;
+                let degree = Math.PI / 2 -  indicatorRad * i;
                 let x = ~~(centerX + Math.cos(degree) * _r);
                 let y = ~~(centerX - Math.sin(degree) * _r);
                 if (i === 0) {
@@ -48,13 +53,24 @@ class Radar{
 
         ctx.stroke();
 
+        this._drawArea();
+    }
+
+    /**
+     * 绘制面积区域
+     * @private
+     */
+    _drawArea() {
+        let {ctx, indicatorCount, data, indicatorRad} = this;
         ctx.beginPath();
         ctx.strokeStyle = '#13bbff';
         ctx.fillStyle = 'rgba(19, 187, 255, 0.7)';
 
+        let startX, startY;
+
         for (let i = 0; i < indicatorCount; i++) {
             let _r = r * data.data[i] / data.indicator[i].max;
-            let degree = Math.PI / 2 -  rad * i;
+            let degree = Math.PI / 2 -  indicatorRad * i;
             let x = ~~(centerX + Math.cos(degree) * _r);
             let y = ~~(centerX - Math.sin(degree) * _r);
             if (i === 0) {
