a9f840c9f948471a64beb74ef45cb7e9cf204885
Vimo <929992114@qq.com>
2018-08-13 16:40:05 +0800

    微信小程序 提示lib

---
+++ js/lib/wx.js
@@ -0,0 +1,368 @@
+const wx = {
+    addCard: () => {
+    },
+    addPhoneContact: () => {
+    },
+    authorize: () => {
+    },
+    canIUse: () => {
+    },
+    canvasGetImageData: () => {
+    },
+    canvasPutImageData: () => {
+    },
+    canvasToTempFilePath: () => {
+    },
+    checkIsSoterEnrolledInDevice: () => {
+    },
+    checkIsSupportSoterAuthentication: () => {
+    },
+    checkSession: () => {
+    },
+    chooseAddress: () => {
+    },
+    chooseImage: () => {
+    },
+    chooseInvoiceTitle: () => {
+    },
+    chooseLocation: () => {
+    },
+    chooseVideo: () => {
+    },
+    clearStorage: () => {
+    },
+    clearStorageSync: () => {
+    },
+    closeBLEConnection: () => {
+    },
+    closeBluetoothAdapter: () => {
+    },
+    closeSocket: () => {
+    },
+    connectSocket: () => {
+    },
+    connectWifi: () => {
+    },
+    createAnimation: () => {
+    },
+    createAudioContext: () => {
+    },
+    createBLEConnection: () => {
+    },
+    createCameraContext: () => {
+    },
+    createCanvasContext: () => {
+    },
+    createContext: () => {
+    },
+    createInnerAudioContext: () => {
+    },
+    createIntersectionObserver: () => {
+    },
+    createLivePlayerContext: () => {
+    },
+    createLivePusherContext: () => {
+    },
+    createMapContext: () => {
+    },
+    createSelectorQuery: () => {
+    },
+    createVideoContext: () => {
+    },
+    createWorker: () => {
+    },
+    downloadFile: () => {
+    },
+    drawCanvas: () => {
+    },
+    getAccountInfoSync: () => {
+    },
+    getAvailableAudioSources: () => {
+    },
+    getBLEDeviceCharacteristics: () => {
+    },
+    getBLEDeviceServices: () => {
+    },
+    getBackgroundAudioManager: () => {
+    },
+    getBackgroundAudioPlayerState: () => {
+    },
+    getBeacons: () => {
+    },
+    getBluetoothAdapterState: () => {
+    },
+    getBluetoothDevices: () => {
+    },
+    getClipboardData: () => {
+    },
+    getConnectedBluetoothDevices: () => {
+    },
+    getConnectedWifi: () => {
+    },
+    getExtConfig: () => {
+    },
+    getExtConfigSync: () => {
+    },
+    getFileInfo: () => {
+    },
+    getHCEState: () => {
+    },
+    getImageInfo: () => {
+    },
+    getLocation: () => {
+    },
+    getLogManager: () => {
+    },
+    getNetworkType: () => {
+    },
+    getRecorderManager: () => {
+    },
+    getSavedFileInfo: () => {
+    },
+    getSavedFileList: () => {
+    },
+    getScreenBrightness: () => {
+    },
+    getSetting: () => {
+    },
+    getShareInfo: () => {
+    },
+    getStorage: () => {
+    },
+    getStorageInfo: () => {
+    },
+    getStorageInfoSync: () => {
+    },
+    getStorageSync: () => {
+    },
+    getSystemInfo: () => {
+    },
+    getSystemInfoSync: () => {
+    },
+    getUpdateManager: () => {
+    },
+    getUserInfo: () => {
+    },
+    getWeRunData: () => {
+    },
+    getWifiList: () => {
+    },
+    hideLoading: () => {
+    },
+    hideNavigationBarLoading: () => {
+    },
+    hideShareMenu: () => {
+    },
+    hideTabBar: () => {
+    },
+    hideTabBarRedDot: () => {
+    },
+    hideToast: () => {
+    },
+    loadFontFace: () => {
+    },
+    login: () => {
+    },
+    makePhoneCall: () => {
+    },
+    navigateBack: () => {
+    },
+    navigateBackMiniProgram: () => {
+    },
+    navigateTo: () => {
+    },
+    navigateToMiniProgram: () => {
+    },
+    notifyBLECharacteristicValueChange: () => {
+    },
+    onAccelerometerChange: () => {
+    },
+    onBLECharacteristicValueChange: () => {
+    },
+    onBLEConnectionStateChange: () => {
+    },
+    onBackgroundAudioPause: () => {
+    },
+    onBackgroundAudioPlay: () => {
+    },
+    onBackgroundAudioStop: () => {
+    },
+    onBeaconServiceChange: () => {
+    },
+    onBeaconUpdate: () => {
+    },
+    onBluetoothAdapterStateChange: () => {
+    },
+    onBluetoothDeviceFound: () => {
+    },
+    onCompassChange: () => {
+    },
+    onGetWifiList: () => {
+    },
+    onHCEMessage: () => {
+    },
+    onMemoryWarning: () => {
+    },
+    onNetworkStatusChange: () => {
+    },
+    onSocketClose: () => {
+    },
+    onSocketError: () => {
+    },
+    onSocketMessage: () => {
+    },
+    onSocketOpen: () => {
+    },
+    onUserCaptureScreen: () => {
+    },
+    onWifiConnected: () => {
+    },
+    openBluetoothAdapter: () => {
+    },
+    openCard: () => {
+    },
+    openDocument: () => {
+    },
+    openLocation: () => {
+    },
+    openSetting: () => {
+    },
+    pageScrollTo: () => {
+    },
+    pauseBackgroundAudio: () => {
+    },
+    pauseVoice: () => {
+    },
+    playBackgroundAudio: () => {
+    },
+    playVoice: () => {
+    },
+    previewImage: () => {
+    },
+    reLaunch: () => {
+    },
+    readBLECharacteristicValue: () => {
+    },
+    redirectTo: () => {
+    },
+    removeSavedFile: () => {
+    },
+    removeStorage: () => {
+    },
+    removeStorageSync: () => {
+    },
+    removeTabBarBadge: () => {
+    },
+    request: () => {
+    },
+    requestPayment: () => {
+    },
+    saveFile: () => {
+    },
+    saveImageToPhotosAlbum: () => {
+    },
+    saveVideoToPhotosAlbum: () => {
+    },
+    scanCode: () => {
+    },
+    seekBackgroundAudio: () => {
+    },
+    sendHCEMessage: () => {
+    },
+    sendSocketMessage: () => {
+    },
+    setBackgroundColor: () => {
+    },
+    setBackgroundTextStyle: () => {
+    },
+    setClipboardData: () => {
+    },
+    setKeepScreenOn: () => {
+    },
+    setNavigationBarColor: () => {
+    },
+    setNavigationBarTitle: () => {
+    },
+    setScreenBrightness: () => {
+    },
+    setStorage: () => {
+    },
+    setStorageSync: () => {
+    },
+    setTabBarBadge: () => {
+    },
+    setTabBarItem: () => {
+    },
+    setTabBarStyle: () => {
+    },
+    setTopBarText: () => {
+    },
+    setWifiList: () => {
+    },
+    showActionSheet: () => {
+    },
+    showLoading: () => {
+    },
+    showModal: () => {
+    },
+    showNavigationBarLoading: () => {
+    },
+    showShareMenu: () => {
+    },
+    showTabBar: () => {
+    },
+    showTabBarRedDot: () => {
+    },
+    showToast: () => {
+    },
+    startAccelerometer: () => {
+    },
+    startBeaconDiscovery: () => {
+    },
+    startBluetoothDevicesDiscovery: () => {
+    },
+    startCompass: () => {
+    },
+    startHCE: () => {
+    },
+    startPullDownRefresh: () => {
+    },
+    startRecord: () => {
+    },
+    startSoterAuthentication: () => {
+    },
+    startWifi: () => {
+    },
+    stopAccelerometer: () => {
+    },
+    stopBackgroundAudio: () => {
+    },
+    stopBeaconDiscovery: () => {
+    },
+    stopBluetoothDevicesDiscovery: () => {
+    },
+    stopCompass: () => {
+    },
+    stopHCE: () => {
+    },
+    stopPullDownRefresh: () => {
+    },
+    stopRecord: () => {
+    },
+    stopVoice: () => {
+    },
+    stopWifi: () => {
+    },
+    switchTab: () => {
+    },
+    updateShareMenu: () => {
+    },
+    uploadFile: () => {
+    },
+    vibrateLong: () => {
+    },
+    vibrateShort: () => {
+    },
+    writeBLECharacteristicValue: () => {
+    },
+};
