8b919387fea20c530a2ff880a357a15deea02b5d
Vimo <929992114@qq.com>
2018-11-08 00:54:52 +0800

    js 异步形式

---
+++ nodejs/test/promiseAsync.js
@@ -0,0 +1,19 @@
+// Macro task & Micro task
+console.log(1);
+setTimeout(() => {
+    console.log(4)
+});
+let p = new Promise((resolve) => {
+    console.log(2);
+    resolve(3)
+});
+
+p.then((res) => {
+    console.log(res);
+    return 3.5
+}).then((res) => {
+    console.log(res)
+});
+console.log(2.5);
+
+
