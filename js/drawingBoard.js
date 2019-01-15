7d311cb1b47ed345c1ec72d908f6b91300c677a3
Vivomo <929992114@qq.com>
2019-01-15 19:12:58 +0800

    画板 历史记录

--- js/drawingBoard.js
+++ js/drawingBoard.js
@@ -5,7 +5,10 @@ function DrawingBoard(cfg) {
 
 DrawingBoard.prototype = {
     constructor: DrawingBoard,
-    defaultCfg: {},
+    defaultCfg: {
+        maxHistorySize: 20
+    },
+    history: [],
     init: function() {
         this.initHtml();
         this.initCanvas();
@@ -19,6 +22,8 @@ DrawingBoard.prototype = {
     },
     initForeground: function() {
         this.foreground = this.cfg.wrap.querySelector('.foreground');
+        this.width = this.foreground.width;
+        this.height = this.foreground.height;
         this.ctx = this.foreground.getContext('2d');
         let {left, top} = this.foreground.getBoundingClientRect();
         this.canvasOffsetX = left;
@@ -27,6 +32,8 @@ DrawingBoard.prototype = {
     },
     initBackground: function() {
         this.background = this.cfg.wrap.querySelector('.background');
+        this.backCtx = this.background.getContext('2d');
+        this.backCtx.globalCompositeOperation = 'lighter';
     },
     initEvent: function() {
         this.initForegroundEvent();
@@ -52,7 +59,8 @@ DrawingBoard.prototype = {
         });
 
         canvas.addEventListener('mouseup', () => {
-            canvas.removeEventListener('mousemove', this.command)
+            canvas.removeEventListener('mousemove', this.command);
+            this.updateHistory();
         });
 
         canvas.addEventListener('touchstart', (e) => {
@@ -65,5 +73,13 @@ DrawingBoard.prototype = {
         canvas.addEventListener('touchend', () => {
             canvas.removeEventListener('touchmove', drawWap)
         });
+    },
+    updateHistory: function () {
+        this.backCtx.drawImage(this.foreground, 0, 0);
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        this.history.push(this.backCtx.getImageData(0, 0, this.width, this.height));
+        if (this.history.length > this.cfg.maxHistorySize) {
+            this.history.unshift();
+        }
     }
 };
