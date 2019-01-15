70c6733ff9ef7551f771026793b8a784233d9974
Vivomo <929992114@qq.com>
2019-01-15 17:20:03 +0800

    画板 画笔功能实现

---
+++ js/drawingBoard.js
@@ -0,0 +1,62 @@
+function DrawingBoard(cfg) {
+    this.cfg = Object.assign({}, this.defaultCfg, cfg);
+    this.init();
+}
+
+DrawingBoard.prototype = {
+    constructor: DrawingBoard,
+    defaultCfg: {},
+    init: function() {
+        this.initHtml();
+        this.initEvent();
+    },
+    initHtml: function() {
+        this.initForeground()
+    },
+    initForeground: function() {
+        this.foreground = this.cfg.wrap.querySelector('.foreground');
+        this.ctx = this.foreground.getContext('2d');
+        let {left, top} = this.foreground.getBoundingClientRect();
+        this.canvasOffsetX = left;
+        this.canvasOffsetY = top;
+        this.command = this.commands.draw;
+    },
+    initEvent: function() {
+        this.initForegroundEvent();
+    },
+    commands: {
+        draw: function ({clientX, clientY}) {
+            let {canvasOffsetX, canvasOffsetY, ctx} = this;
+            ctx.lineCap = 'round';
+            ctx.lineTo(clientX - canvasOffsetX, clientY - canvasOffsetY);
+            ctx.stroke();
+        }
+    },
+    initForegroundEvent: function () {
+        let canvas = this.foreground;
+        let ctx = this.ctx;
+        let {canvasOffsetX, canvasOffsetY} = this;
+        let command = this.command.bind(this);
+        canvas.addEventListener('mousedown', (e) => {
+            let {clientX, clientY} = e;
+            ctx.beginPath();
+            ctx.moveTo(clientX - canvasOffsetX, clientY - canvasOffsetY);
+            canvas.addEventListener('mousemove', command)
+        });
+
+        canvas.addEventListener('mouseup', () => {
+            canvas.removeEventListener('mousemove', command)
+        });
+
+        canvas.addEventListener('touchstart', (e) => {
+            ctx.beginPath();
+            let {clientX, clientY} = e.touches[0];
+            ctx.moveTo(clientX - canvasOffsetX, clientY - canvasOffsetY);
+            canvas.addEventListener('touchmove', drawWap)
+        });
+
+        canvas.addEventListener('touchend', () => {
+            canvas.removeEventListener('touchmove', drawWap)
+        });
+    }
+};
