d6d20cb30aa3efdbdc02e093ab08f25350e98ef8
Vivomo <929992114@qq.com>
2019-01-16 09:19:24 +0800

    添加代码注释

--- js/drawingBoard.js
+++ js/drawingBoard.js
@@ -6,20 +6,37 @@ function DrawingBoard(cfg) {
 DrawingBoard.prototype = {
     constructor: DrawingBoard,
     defaultCfg: {
+        // 历史记录最大保存次数
         maxHistorySize: 20
     },
+    // 操作历史
     history: [],
+    /**
+     * 初始化
+     * html初始化
+     * canvas初始化
+     * 事件初始化
+     */
     init: function() {
         this.initHtml();
         this.initCanvas();
         this.initEvent();
     },
+    /**
+     * 初始化html
+     */
     initHtml: function() {
     },
+    /**
+     * 初始化canvas, 前景和背景
+     */
     initCanvas: function() {
         this.initForeground();
         this.initBackground();
     },
+    /**
+     * 初始化前景canvas
+     */
     initForeground: function() {
         this.foreground = this.cfg.wrap.querySelector('.foreground');
         this.width = this.foreground.width;
@@ -30,15 +47,29 @@ DrawingBoard.prototype = {
         this.canvasOffsetY = top;
         this.command = this.commands.draw.bind(this);
     },
+    /**
+     * 初始化背景canvas
+     */
     initBackground: function() {
         this.background = this.cfg.wrap.querySelector('.background');
         this.backCtx = this.background.getContext('2d');
         this.backCtx.globalCompositeOperation = 'lighter';
     },
+    /**
+     * 初始化绑定事件
+     */
     initEvent: function() {
         this.initForegroundEvent();
     },
+    /**
+     * 画板指令列表
+     */
     commands: {
+        /**
+         * 画笔指令
+         * @param clientX
+         * @param clientY
+         */
         draw: function ({clientX, clientY}) {
             let {canvasOffsetX, canvasOffsetY, ctx} = this;
             ctx.lineCap = 'round';
@@ -46,6 +77,9 @@ DrawingBoard.prototype = {
             ctx.stroke();
         }
     },
+    /**
+     * 前景canvas事件绑定
+     */
     initForegroundEvent: function () {
         let canvas = this.foreground;
         let ctx = this.ctx;
@@ -74,6 +108,9 @@ DrawingBoard.prototype = {
             canvas.removeEventListener('touchmove', drawWap)
         });
     },
+    /**
+     * 更新历史
+     */
     updateHistory: function () {
         this.backCtx.drawImage(this.foreground, 0, 0);
         this.ctx.clearRect(0, 0, this.width, this.height);
