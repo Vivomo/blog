b01263acd26b41fecbc47f937bbd7ef0d63958ad
Vivomo <929992114@qq.com>
2019-01-24 10:53:38 +0800

    添加数据

--- app/grade/q.js
+++ app/grade/q.js
@@ -1,15 +1,36 @@
 export const q = `
-111111
+html
 ---
-111
-123
-134
+label的作用
+图片Alt和title的区别
+html语义化的理解
 
 =====
 
-222222
+CSS
 ---
-222
-234
-244
+CSS视觉上隐藏一个元素(display, visibility, position:left, zIndex, transform, opacity)
+上述方法的区别
+css常用单位
+垂直居中 (position/flex/transform/table-cell)
+清除浮动方法
+
+=====
+
+JS
+---
+typeof(function/number/boolean/object/string/undefined/symbol)
+闭包的优点
+Array常用的遍历方法(map/every/some/forEach/reduce)
+addEventListener第三个参数
+call apply(,[args]) bind
+cmd(node)和amd(require.js)
+
+=====
+
+综合
+---
+通过一个图片url获取其大小
+vue双向绑定的原理
+zepto的点击穿透
 `;
