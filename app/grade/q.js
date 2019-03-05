f43d2b7517f0553ec4c27f83d619acd698a72515
Vivomo <929992114@qq.com>
2019-03-05 11:24:56 +0800

    add q

--- app/grade/q.js
+++ app/grade/q.js
@@ -30,14 +30,19 @@ cmd(node)和amd(require.js)
 localstorage 和 cookie
 let 和 var 的区别
 => 函数和function的区别
+给数组length赋值
+数组去重
 =====
 
 综合
 ---
+http请求和https请求区别(证书,端口,安全性)
+浏览器在使用HTTPS方式与Web服务器通信步骤(请求返回公钥)
 通过一个图片url获取其大小
 vue双向绑定的原理
-vue的路由实现原理 popstate pushstate onhashchange
+单页面应用的路由实现原理 popstate pushstate onhashchange
 zepto的点击穿透
 location.query => object
 判断一个dom元素在可视区
+有没有关注最近React版本的变化
 `;
