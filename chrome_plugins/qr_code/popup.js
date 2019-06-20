17aee0f86c99699f7b06f9df8ae73b8e3744fe6c
Vivomo <929992114@qq.com>
2019-06-20 10:46:04 +0800

    新增自定义 url 转换

--- chrome_plugins/qr_code/popup.js
+++ chrome_plugins/qr_code/popup.js
@@ -1,43 +1,11 @@
 chrome.tabs.query({
     active: true
 }, function (tabs) {
-    let tab = tabs[0],
-        width = 200,
-        height = 200,
-        background = "#ffffff",
-        foreground = "#000000";
-
-    let qrcode	= new QRCode(-1, QRErrorCorrectLevel.H);
-    qrcode.addData(tab.url);
-    qrcode.make();
-    let moduleCount = qrcode.getModuleCount();
-
-    // create canvas element
-    let canvas	= document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    let ctx	= canvas.getContext('2d');
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, width, height);
-
-    // compute tileW/tileH based on options.width/options.height
-    let tileW	= width  / moduleCount;
-    let tileH	= height / moduleCount;
-
-    ctx.fillStyle = foreground;
-    for( let row = 0; row < moduleCount; row++ ){
-        for(let col = 0; col < moduleCount; col++ ){
-            if (!qrcode.isDark(row, col)) {
-                continue;
-            }
-            let w = Math.ceil((col + 1) * tileW) - Math.floor(col * tileW);
-            let h = Math.ceil((row+1)*tileW) - Math.floor(row*tileW);
-            ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);
-        }
-    }
-
+    let tab = tabs[0];
+    let canvas = null;
     let wrap = document.getElementById('qr-code');
-    wrap.appendChild(canvas);
+
+    createQRCode(tab.url);
 
     document.getElementById('download').addEventListener('click', function () {
         let canvas = wrap.querySelector('canvas');
@@ -48,5 +16,51 @@ chrome.tabs.query({
             a.click();
         }, 'image/png');
     });
+
+    document.getElementById('urlInput').addEventListener('keydown', function (e) {
+        if (e.keyCode === 13 && this.value.trim() !== '') {
+            createQRCode(this.value.trim());
+        }
+    });
+
+    function createQRCode(text) {
+        let width = 200,
+            height = 200,
+            background = "#ffffff",
+            foreground = "#000000";
+
+        let qrcode	= new QRCode(-1, QRErrorCorrectLevel.H);
+        qrcode.addData(text);
+        qrcode.make();
+        let moduleCount = qrcode.getModuleCount();
+
+        // create canvas element
+        if (!canvas) {
+            canvas	= document.createElement('canvas');
+            wrap.appendChild(canvas);
+
+        }
+        canvas.width = width;
+        canvas.height = height;
+        let ctx	= canvas.getContext('2d');
+        ctx.fillStyle = background;
+        ctx.fillRect(0, 0, width, height);
+
+        // compute tileW/tileH based on options.width/options.height
+        let tileW = width  / moduleCount;
+        let tileH = height / moduleCount;
+
+        ctx.fillStyle = foreground;
+        for( let row = 0; row < moduleCount; row++ ){
+            for(let col = 0; col < moduleCount; col++ ){
+                if (!qrcode.isDark(row, col)) {
+                    continue;
+                }
+                let w = Math.ceil((col + 1) * tileW) - Math.floor(col * tileW);
+                let h = Math.ceil((row+1)*tileW) - Math.floor(row*tileW);
+                ctx.fillRect(Math.round(col*tileW),Math.round(row*tileH), w, h);
+            }
+        }
+    }
 
 });
