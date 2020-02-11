c5eaada3079763fc79f89d2181bb1b1bdc7af220
Vivomo <929992114@qq.com>
2020-02-11 14:50:22 +0800

    子类逻辑提取, 配置化

--- app/tetris/tetris.js
+++ app/tetris/tetris.js
@@ -1,9 +1,15 @@
 class BaseCeil {
     constructor() {
         this.core = document.createElement('div');
+        this.core.innerHTML = `
+            <div class="i1"></div>
+            <div class="i2"></div>
+            <div class="i3"></div>
+        `;
         this.core.classList.add('core-ceil');
         this.rotateState = 0;
-        this.initX = 5;
+        this.x = 5;
+        this.width = 30;
     }
 
     setChild(index, x, y) {
@@ -67,62 +73,40 @@ class BaseCeil {
             this[k] = this.prevState[k];
         }
     }
+
+    render() {
+        this.core.style.transform = `translate( ${this.x * this.width}px, ${this.y * this.width}px)`;
+        this.subCoordinates[this.rotateState].forEach(([x, y], index) => {
+            this.setChild(index, x, y);
+        });
+    }
+
+
+    init() {
+        this.render();
+    }
 }
 
 class I extends BaseCeil {
     constructor() {
         super();
-        this.core.innerHTML = `
-            <div class="i1"></div>
-            <div class="i2"></div>
-            <div class="i3"></div>
-        `;
         this.children = [...this.core.children];
         this.loopCount = 2;
         this.initY = -3;
-        this.x = this.initX;
-        this.y = this.initY;
+        this.y = -3;
         this.rotateMinX = 1;
         this.rotateMaxX = 7;
         this.rotateMaxY = 17;
-    }
-
-    
-    render() {
-        this.core.style.transform = `translate( ${this.x * 30}px, ${this.y * 30}px)`;
-
-        switch(this.rotateState) {
-            case 0:
-                this.setChild(0, 0, -1);
-                this.setChild(1, 0, 1);
-                this.setChild(2, 0, 2);
-                break;
-            default:
-                this.setChild(0, -1, 0);
-                this.setChild(1, 1, 0);
-                this.setChild(2, 2, 0);    
+        this.subCoordinates = {
+            0: [[0, -1], [0, 1], [0, 2]],
+            1: [[-1, 0], [1, 0], [2, 0]],
         }
     }
 
-    getPoints() {
-        let points = [[this.x, this.y]];
-        switch(this.rotateState) {
-            case 0:
-                points.push([this.x, this.y - 1]);
-                points.push([this.x, this.y + 1]);
-                points.push([this.x, this.y + 2]);
-                break;
-            default:
-                points.push([this.x - 1, this.y]);
-                points.push([this.x + 1, this.y]);
-                points.push([this.x + 2, this.y]);
-        };
-        return points;
-    }
-    
 
-    init() {
-        this.render();
+    getPoints() {
+        return [[this.x, this.y]].concat(this.subCoordinates[this.rotateState]
+            .map(([x, y]) => [this.x + x, this.y + y]));
     }
 
 }
