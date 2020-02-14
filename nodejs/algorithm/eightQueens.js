d55c420a0a11e719f7508061c512e1bfaa835788
Vivomo <929992114@qq.com>
2020-02-14 13:27:43 +0800

    八皇后算法优化

--- nodejs/algorithm/eightQueens.js
+++ nodejs/algorithm/eightQueens.js
@@ -1,68 +1,60 @@
 // 八皇后算法
-const queensNum = 8;
-const max = 63;
 let queens = [0];
+let mark = {
+    x: [0],
+    skew1: [0],
+    skew2: [0]
+}
 let count = 0;
 let resultCount = 0;
 console.time('queen');
 while (queens[0] < 8) {
     count ++;
-    let last = queens[queens.length - 1]
-    if (valid(queens)) {
-        if (queens.length === queensNum) {
+    let lastIndex = queens.length - 1;
+    let last = queens[lastIndex];
+    let y = ~~(last / 8);
+    if (valid(mark)) {
+        if (queens.length === 8) {
             console.log('result', queens, count, ++resultCount)
         } else {
-            if (~~(last / 8) < 7) {
-                queens.push((~~(last / 8) + 1) * 8);
+            if (y < 7) {
+                y++;
+                let newLast = y * 8;
+                queens.push(newLast);
+                mark.x.push(0);
+                mark.skew1.push(y);
+                mark.skew2.push(y);
                 continue;
             }
         }
     }
-    if (last === max ||
+    if ( last % 8 === 7 ||
         (queens.length > 1 && 
-            (~~(last / 8) - ~~(queens[queens.length - 2] / 8) > 1))
+            (y - ~~(queens[queens.length - 2] / 8) > 1))
             ) {
-        queens.pop()
+        queens.pop();
+        mark.x.pop();
+        mark.skew1.pop();
+        mark.skew2.pop();
+        lastIndex--;
     }
-    queens[queens.length - 1]++;
+    queens[lastIndex]++;
+    last = queens[lastIndex];
+    let x = last % 8;
+    y = ~~(last / 8);
+    mark.x[lastIndex] = x;
+    mark.skew1[lastIndex] = x + y;
+    mark.skew2[lastIndex] = y - x;
 }
 console.log('第一行已经试完了, 后面的不可能有解了, 解法个数', resultCount);
 console.log('查找次数', count);
 console.timeEnd('queen');
 
 
-function valid(queens) {
-    let cache = {
-        x: {},
-        skew1: {},
-        skew2: {},
-    };
-
-    return queens.every((queen) => {
-        let x = queen % 8;
-        if (cache.x[x]) {
-            return false
-        } else {
-            cache.x[x] = true
-        }
-
-        let y = ~~ (queen / 8);
-        
-
-        let skew1 = y - x;
-        if (cache.skew1[skew1]) {
-            return false
-        } else {
-            cache.skew1[skew1] = true
-        }
-
-        let skew2 = y + x;
-        if (cache.skew2[skew2]) {
-            return false
-        } else {
-            cache.skew2[skew2] = true
-        }
-        return true
-    });
+function valid(mark) {
+    let index = mark.x.length - 1;
+    return !(mark.x.indexOf(mark.x[index]) !== index ||
+        mark.skew1.indexOf(mark.skew1[index]) !== index ||
+        mark.skew2.indexOf(mark.skew2[index]) !== index); 
 }
 
