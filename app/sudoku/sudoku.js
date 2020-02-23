b82f1ab11f15546dc92d730ce75450aebd8867b4
Vivomo <929992114@qq.com>
2020-02-23 15:04:21 +0800

    点击激活方格

--- app/sudoku/sudoku.js
+++ app/sudoku/sudoku.js
@@ -1,19 +1,41 @@
 const App = {
-    init() {
-        let wrap = document.querySelector('.wrap');
+    set activeCeil(obj){
+        let prevActive = this.wrap.querySelector('.active');
+        if (prevActive) {
+            prevActive.classList.remove('active');
+        }
+        this.wrap.querySelector(`[data-r="${obj.r}"][data-c="${obj.c}"]`).classList.add('active');
+        this._activeCeil = obj;
+    },
+    initHtml () {
+        let wrap = this.wrap = document.querySelector('.wrap');
         let html = new Array(9).fill(null).map((item, outerIndex) => {
             let _html = new Array(9).fill(null).map((innerItem, innerIndex) => {
                 let tempHtml = new Array(9).fill(null).map((temp, tempIndex) => {
                     return `<div class="temp">${tempIndex + 1}</div>`
                 }).join('');
                 return `<div 
-                            data-r="${~~(outerIndex / 3) + 1}" 
+                            data-r="${~~(outerIndex / 3) * 3 + ~~(innerIndex / 3) + 1}" 
                             data-c="${(outerIndex % 3) * 3 + (innerIndex % 3) + 1}" 
                             class="ceil">${tempHtml}</div>`
             }).join('');
             return `<div class="t t${outerIndex + 1}">${_html}</div>`;
         }).join('');
         wrap.innerHTML = html;
+    },
+    initEvent () {
+        this.wrap.addEventListener('click', (e) => {
+            if (e.target.className === 'ceil') {
+                this.activeCeil = {
+                    r: e.target.dataset.r,
+                    c: e.target.dataset.c
+                }
+            }
+        });
+    },
+    init() {
+        this.initHtml();
+        this.initEvent();
     }
 }
 
