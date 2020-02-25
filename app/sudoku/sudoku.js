5bf2fa0d62d79dde2ab5a8a67569f14b3c870797
Vivomo <929992114@qq.com>
2020-02-25 13:22:32 +0800

    优化推导

--- app/sudoku/sudoku.js
+++ app/sudoku/sudoku.js
@@ -140,6 +140,12 @@ const App = {
     },
     setArrCounter(item, counter, rowIndex, colIndex) {
         if (Array.isArray(item)) {
+            if (item.length === 1) {
+                this.activeCeil = {r: rowIndex, c: colIndex};
+                this.setCeil(item[0]);
+                counter[item[0]] = true;
+                return;
+            }
             item.forEach((num) => {
                 if (counter[num]) {
                     counter[num] = 2
@@ -329,4 +335,4 @@ App.init();
 
 let masterData = [[0,2,4],[0,3,6],[0,5,2],[1,0,6],[1,4,3],[1,8,4],[2,1,2],[2,3,4],[2,7,9],[3,0,9],[3,1,8],[3,6,3],[3,7,5],[4,0,1],[4,2,3],[4,7,4],[5,1,6],[5,6,8],[5,8,7],[6,1,3],[6,4,2],[6,7,7],[7,4,6],[7,5,1],[7,8,5],[8,2,9],[8,3,3],[8,6,4]];
 let middleData = [[0,0,4],[0,2,3],[0,5,1],[0,7,2],[1,1,2],[1,2,6],[1,4,7],[1,8,5],[3,0,2],[3,3,8],[3,5,6],[3,7,3],[4,0,3],[4,1,6],[4,5,9],[5,3,2],[5,7,9],[5,8,4],[6,0,7],[6,3,3],[6,6,4],[6,7,8],[7,1,5],[7,4,8],[7,6,3],[7,8,2]];
-App.setDefaultData(middleData);
+// App.setDefaultData(middleData);
