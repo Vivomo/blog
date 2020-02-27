b16bc6d2c37e9d7d1fb6b6bfdb14368ef5efe3f2
Vivomo <929992114@qq.com>
2020-02-27 21:52:02 +0800

    编辑模式逻辑完成

--- app/pullbox/pullbox.js
+++ app/pullbox/pullbox.js
@@ -8,7 +8,36 @@ let App = {
         });
         document.querySelector('table').addEventListener('click', (e) => {
             let td = e.target;
-            td.classList.toggle(this.type);
+            if (td.classList.contains(this.type)) {
+                td.classList.remove(this.type);
+                return;
+
+            }
+            switch(this.type) {
+                case 'person':
+                    let person = document.querySelector('.person');
+                    if (person) {
+                        person.className = '';
+                    }
+                    td.className = this.type;
+                    break;
+                case 'target':
+                    if (td.className === 'box') {
+                        td.classList.add(this.type);
+                    } else {
+                        td.className = this.type;
+                    }
+                    break;
+                case 'box':
+                    if (td.className === 'box') {
+                        td.classList.add(this.type);
+                    } else {
+                        td.className = this.type;
+                    }
+                    break;
+                case 'wall':
+                    td.className = this.type;
+            }
         });
     },
     init() {
