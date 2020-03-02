c51929224690df2e327eedda576a40b31d23bbf9
Vimo <929992114@qq.com>
2020-03-03 04:14:50 +0800

    移动逻辑完善

--- app/pullbox/pullbox.js
+++ app/pullbox/pullbox.js
@@ -13,32 +13,44 @@ let App = {
             return null;
         }
         let {x, y} = person.dataset;
-        return {x, y};
+        return {x: ~~x, y: ~~y};
     },
     nextPoint(point, direction) {
         let {x, y} = point;
         if (direction % 2 === 0) {
-            x = point.x + direction - 1;
+            y = point.y + direction - 1;
         } else {
-            y = point.y + direction - 2;
+            x = point.x + direction - 2;
         }
         return {x, y};
     },
-    move(point, direction) {
+    move(direction) {
+        let point = this.getPersonPoint();
+        if (!point) {
+            return;
+        }
         let curElem = this.getPointElem(point);
         let next = this.nextPoint(point, direction);
         let nextElem = this.getPointElem(next);
         let next2 = this.nextPoint(next, direction);
         let next2Elem = this.getPointElem(next2);
 
-        if (this.isWall(nextElem)) {
+        if (!nextElem || !next2Elem || this.isWall(nextElem)) {
             return;
         }
         if (this.isBox(nextElem)) {
             if (this.isWall(next2Elem)) {
                 return;
             }
+            this.moveElemTo(nextElem, next2);
         }
+        this.moveElemTo(curElem, next);
+    },
+    moveElemTo(elem, point) {
+        let nextElem = this.getPointElem(point);
+        let type = this.isBox(elem) ? Type.box : Type.person;
+        elem.classList.remove(type);
+        nextElem.classList.add(type);
     },
     isWall(elem) {
         return elem.classList.contains(Type.wall);
@@ -103,20 +115,16 @@ let App = {
         });
 
         window.addEventListener('keydown', (e) => {
-            let point = this.getPersonPoint();
-            if (!point) {
-                return;
-            }
             let direction = {
                 ArrowLeft: 0,
                 ArrowUp: 1,
                 ArrowRight: 2,
                 ArrowDown: 3,
             }[e.key];
-            if (!direction) {
+            if (direction === undefined) {
                 return;
             }
-            this.move(point, direction);
+            this.move(direction);
         });
     },
     init() {
