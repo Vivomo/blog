ad887a6ab3fe10b82bf70b611c68080193dada5a
vivomo <929992114@qq.com>
2020-09-12 22:39:42 +0800

    算法优化

--- nodejs/canvas/badApple.js
+++ nodejs/canvas/badApple.js
@@ -13,15 +13,12 @@ let imgPath = path.join(__dirname, '../../ignore/6186.jpg');
 let compress = (arr) => {
     let result = [];
     let temp = 0;
-    arr.forEach((row) => {
-        row.forEach((item, index) => {
-            temp += item << (index % 16);
-            if (index % 16 === 15) {
-                // console.log(temp)
-                result.push(String.fromCharCode(temp));
-                temp = 0;
-            }
-        });
+    arr.forEach((item, index) => {
+        temp += item << (index % 16);
+        if (index % 16 === 15) {
+            result.push(String.fromCharCode(temp));
+            temp = 0;
+        }
     });
     console.log(result.length)
     return result.join('');
@@ -33,7 +30,8 @@ loadImage(imgPath).then((img) => {
     let wBlockCount = Math.ceil(width / pixelValue);
     let hBlockCount = Math.ceil(height / pixelValue);
 
-    let letterArea = new Array(hBlockCount).fill(0).map(_ => new Array(wBlockCount));
+    // let letterArea = new Array(hBlockCount).fill(0).map(_ => new Array(wBlockCount));
+    let letterArea = [];
 
     let wRemainder = width % pixelValue;
     let hRemainder = height % pixelValue;
@@ -42,28 +40,23 @@ loadImage(imgPath).then((img) => {
 
     let getImageData =  (startX = 0, startY = 0, width, height) => {
         return ctx.getImageData(startX, startY, width, height);
-    }
+    };
 
-    for (let i = 0; i < wBlockCount; i++) {
-        for (let j = 0; j < hBlockCount; j++) {
-            // updateImgData(i, j)
-            // setTimeout(() => {
-                let w = i === wBlockCount - 1 ? lastWidth : pixelValue;
-                let h = j === hBlockCount - 1 ? lastHeight : pixelValue;
-                let imageData = getImageData(i * pixelValue, j * pixelValue, w, h);
-                let color = 0;
-                imageData.data.forEach((item, index) => {
-                    if (index % 4 !== 3) {
-                        color += item;
-                    }
-                });
-
-                letterArea[j][i] = color / 3 / (imageData.data.length / 4) > 128 ? 0 : 1;
-
-            // });
+    for (let j = 0; j < hBlockCount; j++) {
+        for (let i = 0; i < wBlockCount; i++) {
+            let w = i === wBlockCount - 1 ? lastWidth : pixelValue;
+            let h = j === hBlockCount - 1 ? lastHeight : pixelValue;
+            let imageData = getImageData(i * pixelValue, j * pixelValue, w, h);
+            let color = 0;
+            imageData.data.forEach((item, index) => {
+                if (index % 4 !== 3) {
+                    color += item;
+                }
+            });
+            let block = color / 3 / (imageData.data.length / 4) > 128 ? 0 : 1;
+            letterArea.push(block);
         }
     }
-    // let result = letterArea.map(item => item.join(''));
     let result = compress(letterArea);
     console.log(result);
 
