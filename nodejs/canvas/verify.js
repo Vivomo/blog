dfde862d60ea8a76761b99a61cb8d62e3166d922
vivomo <929992114@qq.com>
2020-09-18 01:50:35 +0800

    压缩验证

--- nodejs/canvas/verify.js
+++ nodejs/canvas/verify.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const {createCanvas, loadImage} = require('canvas');
+const {compress, RLE} = require('./utils');
 
 let basePath = 'D:\\AppData\\bad_apple\\';
 let pixelValue = 10;
@@ -15,6 +16,9 @@ let getImageData =  (startX = 0, startY = 0, width, height) => {
 
 function verify(imgIndex) {
     let imgPath = basePath + imgIndex + '.jpg';
+    let w = width / pixelValue;
+    let h = height / pixelValue;
+
     loadImage(imgPath).then((img) => {
         ctx.drawImage(img, 0, 0);
 
@@ -46,12 +50,24 @@ function verify(imgIndex) {
         }
         return letterArea;
     }).then((letterArea) => {
-        let w = width / pixelValue;
-        let h = height / pixelValue;
+
         for (let i = 0; i < h; i++) {
             console.log(letterArea.slice(i * w  , (i + 1) * w).join(''));
         }
         return letterArea;
+    }).then((letterArea) => {
+        console.log('\n\ncompress\n\n')
+        let compressLetter = compress(letterArea);
+        // for (let i = 0; i < h; i++) {
+        //     console.log(compressLetter.slice(i * w / 8, (i + 1) * w / 8)
+        //         .map(item => item.toString(2).padStart(8, '0')).join(''))
+        // }
+        console.log(compressLetter);
+        return compressLetter;
+    }).then((data) => {
+        let rleData = RLE(data);
+        console.log(rleData);
+        return rleData;
     });
 }
 
