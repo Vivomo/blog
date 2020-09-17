a1fe54079b079c703b60959c057804880d2241be
vivomo <929992114@qq.com>
2020-09-18 04:38:53 +0800

    压缩优化

--- nodejs/canvas/compressRLE.js
+++ nodejs/canvas/compressRLE.js
@@ -12,13 +12,13 @@ let zipFrames = [];
 for (let i = 0; i < frameCount; i++) {
     let frame = frames.slice(i * pxBtCount, (i + 1) * pxBtCount);
     let [startPx, ...zipFrame] = RLE(frame);
-    if (zipFrame.length * 2 > pxBtCount) {
+    if (zipFrame.length / 2 > pxBtCount) {
         zipFrames.push([0, ...frame]);
     } else {
-        zipFrames.push([(startPx << 7) | 1, zipFrame.length, zipFrame]);
+        zipFrames.push([(startPx << 7) | 1, zipFrame.length, ...zipFrame]);
     }
 }
-let flatData= zipFrames.flat(2);
+let flatData= zipFrames.flat(1);
 let data = new Int16Array(flatData);
 fs.writeFileSync('D:\\code\\git\\blog\\ignore\\bufferRLE.b', Buffer.from(data.buffer));
 
