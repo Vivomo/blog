148ae29b180a6496f8760c9f230023d22877ed24
vivomo <929992114@qq.com>
2020-09-18 01:18:21 +0800

    基础验证测试

---
+++ nodejs/canvas/verify.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const {createCanvas, loadImage} = require('canvas');
+
+let basePath = 'D:\\AppData\\bad_apple\\';
+let pixelValue = 10;
+let width = 960;
+let height = 720;
+
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+
+let getImageData =  (startX = 0, startY = 0, width, height) => {
+    return ctx.getImageData(startX, startY, width, height);
+};
+
+function verify(imgIndex) {
+    let imgPath = basePath + imgIndex + '.jpg';
+    loadImage(imgPath).then((img) => {
+        ctx.drawImage(img, 0, 0);
+
+        let wBlockCount = Math.ceil(width / pixelValue);
+        let hBlockCount = Math.ceil(height / pixelValue);
+
+        let letterArea = [];
+
+        let wRemainder = width % pixelValue;
+        let hRemainder = height % pixelValue;
+        let lastWidth = wRemainder === 0 ? pixelValue : wRemainder;
+        let lastHeight = hRemainder === 0 ? pixelValue : hRemainder;
+
+
+        for (let j = 0; j < hBlockCount; j++) {
+            for (let i = 0; i < wBlockCount; i++) {
+                let w = i === wBlockCount - 1 ? lastWidth : pixelValue;
+                let h = j === hBlockCount - 1 ? lastHeight : pixelValue;
+                let imageData = getImageData(i * pixelValue, j * pixelValue, w, h);
+                let color = 0;
+                imageData.data.forEach((item, index) => {
+                    if (index % 4 !== 3) {
+                        color += item;
+                    }
+                });
+                let block = color / 3 / (imageData.data.length / 4) > 128 ? 0 : 1;
+                letterArea.push(block);
+            }
+        }
+        return letterArea;
+    }).then((letterArea) => {
+        let w = width / pixelValue;
+        let h = height / pixelValue;
+        for (let i = 0; i < h; i++) {
+            console.log(letterArea.slice(i * w  , (i + 1) * w).join(''));
+        }
+        return letterArea;
+    });
+}
+
+verify(67);
