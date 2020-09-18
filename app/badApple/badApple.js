b370fd6b62bad28b07af8afcf5e3e7872b8d4eba
vivomo <929992114@qq.com>
2020-09-18 23:01:36 +0800

    引入开源zip

--- app/badApple/badApple.js
+++ app/badApple/badApple.js
@@ -1,81 +1,40 @@
-const xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let frames = unzip(xhr.response);
-        draw(frames);
+JSZipUtils.getBinaryContent('buffer.zip', function(err, data) {
+    if(err) {
+        throw err; // or handle err
     }
-};
-xhr.responseType = 'arraybuffer';
-xhr.open('GET', 'bufferRLE.b', true);
-xhr.send(null);
+    JSZip.loadAsync(data).then(function (zip) {
+        return zip.files["buffer.b"].async('uint8array');
+    }).then((data) => {
+        draw(data);
+    });
+});
 
-const isZipFrame = 1;
 const pxBtCount = 864;
 
-let unzip = (resp) => {
-    let framesData = new Int16Array(resp);
-    let unzipFrames = [];
-    let cursor = 0;
-    while (cursor < framesData.length) {
-        let curFrameData = framesData[cursor];
-        if (curFrameData & isZipFrame) {
-            // let startPx = (curFrameData >> 7) & 1;
-            let endIndex = cursor + 2 + framesData[cursor + 1];
-            let frame = framesData.slice(cursor, endIndex);
-            unzipFrames.push(frame);
-            cursor = endIndex;
-        } else {
-            let endIndex = cursor + 1 + pxBtCount / 2;
-            let frame = framesData.slice(cursor + 1, endIndex);
-            unzipFrames.push(frame);
-            cursor = endIndex;
-        }
-    }
-    return unzipFrames;
-};
-
-let unzipFrame = (startPx, countData) => {
-    let curPx = startPx;
-    return Array.from(countData).map((count) => {
-         let data = new Array(count).fill(curPx);
-         curPx = curPx ? 0 : 1;
-         return data;
-    }).flat(1);
-};
 
 let restore = (frameData) => {
     let result = [];
     for (let i = 0; i < frameData.length; i++) {
         for (let j = 7; j >= 0; j--) {
-            result.push((frameData >> j) & 1);
+            result.push((frameData[i] >> j) & 1);
         }
     }
     return result;
 };
 
-
 let apple = document.getElementById('apple');
 let ctx = apple.getContext('2d');
 
-// ctx.beginPath();
 ctx.fillStyle = '#00aa00';
-ctx.font = "10px Microsoft Yahei"
-// ctx.stroke();
+ctx.font = "10px Arial";
+
 
 let draw = (frames, index = 0) => {
     if (index === frames.length) {
         return;
     }
-    let frame = frames[index];
-    let data = frame[0];
-    let result;
-    if (data & isZipFrame) {
-        let start = (data >> 7) & 1;
-        result = unzipFrame(start, frame.slice(2));
-    } else {
-        result = restore(frame.slice(1));
-    }
+    let result = restore(frames.slice(index * pxBtCount, (index + 1) * pxBtCount));
+
     ctx.clearRect(0, 0, 960, 720);
     for (let i = 1; i <= 72; i++) {
         ctx.fillText(result.slice((i - 1) * 96, i * 96).join(' '), 0, i * 10);
