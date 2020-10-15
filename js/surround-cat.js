be518633c7f6eea830bcc06b3dcd1b938863bde7
vivomo <929992114@qq.com>
2020-10-16 01:50:17 +0800

    surround cat init & build map

---
+++ js/surround-cat.js
@@ -0,0 +1,15 @@
+
+let App = {
+    wrap: document.getElementById('wrap'),
+    init() {
+        let col = 11;
+        let row = 11;
+        let html = new Array(row).fill(0).map(_ => {
+            let items = '<div class="point"></div>'.repeat(col);
+            return `<div class="row">${items}</div>`;
+        }).join('');
+        this.wrap.innerHTML = html;
+    }
+};
+
+App.init();
