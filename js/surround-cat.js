145008bed7868833e2444d19cb1fdd2a81cbc928
vivomo <929992114@qq.com>
2020-10-20 09:56:31 +0800

    添加移动方法

--- js/surround-cat.js
+++ js/surround-cat.js
@@ -1,6 +1,18 @@
 
 let App = {
     wrap: document.getElementById('wrap'),
+    colElem: null,
+    rowElem: null,
+    cat: null,
+    catWidth: null,
+    catHeight: null,
+    ceilWidth: null,
+    ceilHeight: null,
+    move(x, y) {
+        let target = this.rowElem[y].children[x];
+        this.cat.style.left = target.offsetLeft - this.catWidth / 2 + this.ceilWidth / 2 + 'px';
+        this.cat.style.top = target.offsetTop - this.catHeight + this.ceilHeight / 2 + 'px';
+    },
     init() {
         let col = 11;
         let row = 11;
@@ -9,6 +21,16 @@ let App = {
             return `<div class="row">${items}</div>`;
         }).join('');
         this.wrap.innerHTML = html + '<div class="cat"></div>';
+
+        this.rowElem = this.wrap.querySelectorAll('.row');
+        this.cat = this.wrap.querySelector('.cat');
+        this.catWidth = this.cat.offsetWidth;
+        this.catHeight = this.cat.offsetHeight;
+        let ceil = this.wrap.querySelector('.point');
+        this.ceilWidth = ceil.offsetWidth;
+        this.catHeight = ceil.offsetHeight;
+
+        this.move(5, 5);
     }
 };
 
