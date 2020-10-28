5e41751f2b036ddd0acc1e4f38e40425fa085eb8
vivomo <929992114@qq.com>
2020-10-29 03:55:33 +0800

    代码精简

--- src/js/orochi.js
+++ src/js/orochi.js
@@ -1,11 +1,8 @@
 let img = document.getElementById('orochi');
 let canvas = document.getElementById('canvas');
-let {width, height} = img;
-canvas.width = width;
-canvas.height = height;
 let ctx = canvas.getContext('2d');
 ctx.drawImage(img, 0, 0);
-let data = ctx.getImageData(0, 0, width, height).data;
+let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 let indexList = [];
 for (let i = 0, l = data.length; i < l; i += 4) {
     if (data[i] !== 255) {
@@ -13,17 +10,11 @@ for (let i = 0, l = data.length; i < l; i += 4) {
     }
 }
 setInterval(() => {
-    let imgData = ctx.getImageData(0, 0, width, height);
-    let data = imgData.data.fill(0);
+    let imgData = ctx.createImageData(canvas.width, canvas.height);
     for (let index of indexList) {
-        let random = Math.random();
-        if (random < 0.3) {
-            index -= 4;
-        } else if (random > 0.7) {
-            index += 4;
-        }
-        data[index + 1] = 128;
-        data[index + 3] = 255;
+        index += (~~(Math.random() * 3) - 1) * 4;
+        imgData.data[index + 1] = 128;
+        imgData.data[index + 3] = 255;
     }
     ctx.putImageData(imgData, 0, 0)
 }, 300);
