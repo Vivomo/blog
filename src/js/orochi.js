cdd7cf5af953f6516ca3c4fdb4287c0fa2ea2ded
vivomo <929992114@qq.com>
2020-10-29 17:18:10 +0800

    Orochi像素动画 更新地址 和加载逻辑

--- src/js/orochi.js
+++ src/js/orochi.js
@@ -1,20 +1,22 @@
-let img = document.getElementById('orochi');
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-ctx.drawImage(img, 0, 0);
-let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-let indexList = [];
-for (let i = 0, l = data.length; i < l; i += 4) {
-    if (data[i] !== 255) {
-        indexList.push(i);
+window.onload = () => {
+    let img = document.getElementById('orochi');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    let indexList = [];
+    for (let i = 0, l = data.length; i < l; i += 4) {
+        if (data[i] !== 255) {
+            indexList.push(i);
+        }
     }
+    setInterval(() => {
+        let imgData = ctx.createImageData(canvas.width, canvas.height);
+        for (let index of indexList) {
+            index += (~~(Math.random() * 3) - 1) * 4;
+            imgData.data[index + 1] = 128;
+            imgData.data[index + 3] = 255;
+        }
+        ctx.putImageData(imgData, 0, 0)
+    }, 300);
 }
-setInterval(() => {
-    let imgData = ctx.createImageData(canvas.width, canvas.height);
-    for (let index of indexList) {
-        index += (~~(Math.random() * 3) - 1) * 4;
-        imgData.data[index + 1] = 128;
-        imgData.data[index + 3] = 255;
-    }
-    ctx.putImageData(imgData, 0, 0)
-}, 300);
