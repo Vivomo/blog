562d0a41bcfcfb7d082fa0d1b1b7ec588a49611d
vivomo <929992114@qq.com>
2020-10-30 23:41:10 +0800

    移除无用的文件

--- eg_build/config.js
+++
@@ -1,7 +0,0 @@
-var webroot = '../compressed'; // webroot
-
-exports.webroot = webroot;
-exports.websrcroot = webroot + '/src';
-exports.webgenroot = webroot + '/gen';
-exports.webassetsroot = webroot + '/assets';
-exports.webtemproot = webroot + '/temp';
