987c2a5ab8c68250af6783b20d3c3aabc13b27fd
vivomo <929992114@qq.com>
2020-11-02 02:44:27 +0800

    orochi添加背景

--- src/js/orochi.js
+++ src/js/orochi.js
@@ -25,4 +25,22 @@ window.onload = () => {
     setTimeout(() => {
         canvas.style.opacity = '1';
     }, 400);
+
+    let q = document.getElementById('canvas-bg'),
+        c = q.getContext('2d'),
+        w = q.width = document.body.clientWidth,
+        h = q.height = window.innerHeight,
+        p = new Array(256).join('1').split('');
+
+    setInterval(function () {
+        c.fillStyle = 'rgba(0,0,0,0.05)';
+        c.fillRect(0, 0, w, h);
+        c.fillStyle = 'rgba(0,255,0,1)';
+        p = p.map((v, i) => {
+            let r = Math.random();
+            c.fillText(String.fromCharCode(Math.floor(2720 + r * 33)), i * 10, v);
+            v += 10;
+            return v > 768 + r * 1e4 ? 0 : v
+        })
+    }, 33);
 }
