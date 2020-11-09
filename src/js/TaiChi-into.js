ca75154a5315a19796180edcd578349437bea2bc
vivomo <929992114@qq.com>
2020-11-09 17:06:01 +0800

    旋转逻辑和 添加球

--- src/js/TaiChi-into.js
+++ src/js/TaiChi-into.js
@@ -11,7 +11,51 @@ let App = (() => {
     container.style.width = containerWidth + 'px';
     TaiChi.style.width = TaiChi.style.height = TaiChiSize + 'px';
 
-    return {
+    let _degree = 0;
+    let dangerLine = {};
+    let interval = 0;
+    let ballDirection = 1;
 
+    return {
+        rotateDirection: 1,
+        set degree(value) {
+            _degree = value;
+            TaiChi.style.transform = `rotate(${_degree}deg)`;
+        },
+        get degree() {
+            return _degree;
+        },
+        toggleDirection() {
+            this.rotateDirection = this.rotateDirection === 1 ? -1 : 1;
+        },
+        rotate() {
+            this.degree += this.rotateDirection * 6;
+        },
+        initDangerLine() {
+            let bound = TaiChi.getBoundingClientRect();
+            dangerLine.top = bound.top - ballSize;
+            dangerLine.bottom = bound.bottom + ballSize;
+
+        },
+        addBall() {
+            let ball = document.createElement('div');
+            ball.className = `ball
+                                ${ballDirection === 1 ? 'from-top' : 'from-bottom'} 
+                                ${Math.random() < 0.5 ? 'black' : 'white'}`;
+            container.appendChild(ball);
+        },
+        start() {
+            this.initDangerLine();
+            this.addBall();
+            interval = setInterval(() => {
+                this.rotate();
+            }, 16);
+        }
     }
 })();
+
+document.body.addEventListener('click', () => {
+    App.toggleDirection();
+});
+
+App.start();
