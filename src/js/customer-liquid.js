07f7b010e2fb2c8a96b8b567be61561bc95909a0
vivomo <929992114@qq.com>
2020-11-11 03:20:42 +0800

    基础样式

--- src/js/customer-liquid.js
+++ src/js/customer-liquid.js
@@ -17,6 +17,56 @@ class Liquid{
     }
 }
 
+class Pen{
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+    draw(liquid, x, y) {
+        let ctx = this.ctx;
+
+
+
+        ctx.fillStyle = '#8f430708';
+        for (let i = 0; i < 10; i++) {
+            ctx.beginPath();
+            ctx.arc(x, y, 100 + i, 0, Math.PI * 2);
+            ctx.fill();
+        }
+
+        // for (let i = 0; i < 10; i++) {
+        //     ctx.beginPath();
+        //     ctx.arc(x, y, 100 - 2 * i, 0, Math.PI * 2);
+        //     ctx.fill();
+        // }
+
+        for (let i = 0; i < 30; i++) {
+            ctx.beginPath();
+            ctx.arc(x, y, 110 - i, 0, Math.PI * 2);
+            ctx.fill();
+        }
+
+        let radial = ctx.createRadialGradient(100,100,20,120,120,50);
+        radial.addColorStop(0,'#bf4407');
+        radial.addColorStop(0.9,'#8f4307');  // 颜色值 #FF0188 == rgba(255,1,136,1)
+        radial.addColorStop(1,'rgba(255,1,136,0)');
+
+        ctx.fillStyle = radial;
+        ctx.beginPath();
+        ctx.fillRect(0,0,500,500);
+        // ctx.arc(300, 300, 110, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
 let canvas = document.querySelector('#canvas');
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
 let ctx = canvas.getContext('2d');
 
+let pen = new Pen(ctx);
+
+pen.draw(null, 800, 400);
