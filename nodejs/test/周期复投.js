35c95f1cb0020bf95799da2ee533db49d45ec086
vivomo <929992114@qq.com>
2021-03-20 18:46:01 +0800

    周期复投

--- nodejs/test/周期复投.js
+++ nodejs/test/周期复投.js
@@ -1,4 +1,5 @@
-let createProduct = (cost, dayIncome, index) => (Object.freeze({cost, dayIncome, remainingPeriod: 90, index}));
+let {nextPermutation} = require('../util/MathUtils');
+let createProduct = (cost, dayIncome) => (Object.freeze({cost, dayIncome}));
 let limit = {
     100000: 1,
     10000: 1,
@@ -9,14 +10,25 @@ let limit = {
     10: 8
 };
 let products = [
-    createProduct(100000, 1520, 0),
-    createProduct(10000, 148, 1),
-    createProduct(5000, 72, 2),
-    createProduct(1000, 14, 3),
-    createProduct(500, 6.8, 4),
-    createProduct(100, 1.32, 5),
-    createProduct(10, 0.132, 6),
+    createProduct(100000, 1520),
+    createProduct(10000, 148),
+    createProduct(5000, 72),
+    createProduct(1000, 14),
+    createProduct(500, 6.8),
+    createProduct(100, 1.32),
+    createProduct(10, 0.132),
 ];
+
+let productsMap = {
+    100000: createProduct(100000, 1520),
+    10000: createProduct(10000, 148),
+    5000: createProduct(5000, 72),
+    1000: createProduct(1000, 14),
+    500: createProduct(500, 6.8),
+    100: createProduct(100, 1.32),
+    10: createProduct(10, 0.132),
+};
+
 
 let getValidProducts = (elem) => {
     let index = elem.products.length ? elem.products[elem.products.length - 1].index : 0;
@@ -79,9 +91,73 @@ let combination = [
     root
 ];
 
+let sum = (pre, cur) => pre + cur.dayIncome;
+
+let getIncome = (arr) => {
+    let proList = [
+        productsMap[10000]
+    ];
+    let buyIndex = 0;
+    let score = 0;
+    for (let i = 1; i <= 47; i++) {
+        score += proList.reduce(sum, 0);
+        while (score > arr[buyIndex]) {
+            let pro = productsMap[arr[buyIndex]];
+            proList.push(pro);
+            score -= arr[buyIndex] - pro.dayIncome;
+            buyIndex++;
+            if (buyIndex === arr.length) {
+                return {
+                    score,
+                    day: i
+                }
+            }
+        }
+    }
+    return {};
+}
+
+let test = [100, 10, 10, 10, 10, 100, 10, 10, 10, 10, 100, 100, 500, 500, 1000, 1000, 5000];
+let test2 = [100, 100, 1000, 10, 500, 500, 100, 1000, 10, 10, 10, 10, 10, 10, 10, 5000, 100];
+
+let arr = [10, 10, 10, 10, 10, 10, 10, 10, 100, 100, 100, 100, 500, 500, 1000, 1000, 5000];
+
+// 172972800
 
 
-getMaxScore(1);
-console.log(combination.length);
-// let score = combination.sort((a, b) => a.score - b.score).map(item => item.score);
-// console.log(score)
+let minDay = 47;
+let bestScore = 0;
+let bestArr = [];
+console.time('a')
+// 1729728
+for (let index = 0; index < 17297280; index++) {
+    let proList = [
+        productsMap[10000]
+    ];
+    let buyIndex = 0;
+    let score = 0;
+    for (let i = 1; i <= minDay; i++) {
+        score += proList.reduce(sum, 0);
+        while (score > arr[buyIndex]) {
+            let pro = productsMap[arr[buyIndex]];
+            proList.push(pro);
+            score -= arr[buyIndex] - pro.dayIncome;
+            buyIndex++;
+            if (buyIndex === arr.length) {
+                if (i < minDay) {
+                    minDay = i;
+                    bestScore = score;
+                    bestArr = proList
+                } else if (score > bestScore) {
+                    bestScore = score;
+                    bestArr = proList
+                }
+                break;
+            }
+        }
+    }
+    nextPermutation(arr)
+}
+
+console.log(minDay, bestScore, bestArr);
+console.timeEnd('a')
