1e5ce2a9473443f1173ca5ae9eea0ab6e37e95ff
Vivomo <929992114@qq.com>
2021-05-19 14:35:53 +0800

    ts transform

--- nodejs/replace/replace.js
+++ nodejs/replace/replace.js
@@ -1,18 +1,47 @@
 const mapStream = require('map-stream');
 const vfs = require('vinyl-fs');
 const glob = require('glob');
-
+const fs = require('fs');
 const cfg = {
     baseDir: '',
 
 };
 
-const log = (file) => {
-    console.log(file.path);
-}
+const jsonStr = fs.readFileSync('./ignore/zh.json', 'utf8');
+const jsonObj = JSON.parse(jsonStr);
 
-let r = glob('../*.js', {}, (err, files) => {
-    console.log(files)
+const targetReg = /formatMessage\(/g;
+const emptyReg = /\s/;
+
+fs.readFile('./ignore/index.tsx', 'utf8', (err, content) => {
+    if (err) {
+        console.error(err)
+    } else {
+        let indexList = [...content.matchAll(targetReg)].map(item => item.index);
+        console.log(indexList);
+    }
 });
 
-console.log(r)
+const transformIndexList = (indexList, content) => {
+    return indexList.map((targetIndex) => {
+        let beforeIndex = targetIndex - 1;
+        while (beforeIndex >= 0) {
+            if (emptyReg.test(content[beforeIndex])) {
+                beforeIndex--;
+                continue;
+            }
+            
+        }
+    });
+};
+
+
+// const log = (file) => {
+//     console.log(file.path);
+// }
+//
+// let r = glob('../*.js', {}, (err, files) => {
+//     console.log(files)
+// });
+//
+// console.log(r)
