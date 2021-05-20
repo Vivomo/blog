b9816a3678a5cd73a791322c04426e0b09eec87a
Vivomo <929992114@qq.com>
2021-05-20 18:27:13 +0800

    transform script

--- nodejs/replace/example.tsx
+++ nodejs/replace/example.tsx
@@ -3,19 +3,32 @@ import { format } from 'format';
 
 const Example = () => {
 
+    let obj = {
+        value: format({ id: 'value'}),
+        data: format({ id: 'obj-data'}, { value: 1, num: 2 })
+    }
+
+    let easy = 'easy';
+
     alert(format({id: 'alert'}))
 
     let msg = format({id: 'msg'});
 
     return (
-        <div title={format({id: 'title'})}>
+        <div
+            title={format({id: 'title'})}
+            data-msg={format({id: 'data-msg'}, {name: 'data-msg'})}
+        >
             {
                 format({
                     id: '123'
                 }, {
-                    name: 'name'
+                    easy,
+                    name: 'name',
+                    num: 1 + 2 + 3
                 })
             }
+            <span>{format({ id: '1234'})}</span>
         </div>
     );
 };
--- nodejs/replace/transform.js
+++ nodejs/replace/transform.js
@@ -4,19 +4,50 @@ const fs = require('fs');
 // https://ts-ast-viewer.com/
 // https://zhuanlan.zhihu.com/p/145278299
 
+const fileContent = fs.readFileSync('./example.tsx', 'utf8');
 const sourceFile = ts.createSourceFile(
     'example',
-    fs.readFileSync('./example.tsx', 'utf8'),
-    ts.ScriptTarget.ES2015,
-    /*setParentNodes */ true
+    fileContent,
+    ts.ScriptTarget.Latest,
+    /*setParentNodes */ true,
+    ts.ScriptKind.TSX
 );
 
+let count = 0;
 function changesVisitor (node) {
-    if (node.kind === 204) {
-        console.log(node)
+    if (node.kind === ts.SyntaxKind.Identifier
+        && node.escapedText === 'format'
+        && node.parent.kind === ts.SyntaxKind.CallExpression
+    ) {
+        console.log('id:', node.parent.arguments[0].properties[0].initializer.text);
+        let obj = {};
+        if (node.parent.arguments[1]) {
+            let props = node.parent.arguments[1].properties;
+            props.forEach((prop) => {
+                let key = prop.name.escapedText.trim();
+                let value;
+                if (prop.kind === ts.SyntaxKind.ShorthandPropertyAssignment) {
+                    value = key;
+                } else {
+                    value = fileContent.substring(prop.initializer.pos, prop.initializer.end).trim();
+                }
+                if (!prop.initializer) {
+                    // console.log(prop);
+                }
+                obj[key] = value;
+
+            });
+            console.log(obj)
+        }
+        // console.log(node)
+        count++;
+        // console.log(fileContent.substring(node.pos, node.end));
+        console.log(fileContent.substring(node.parent.pos, node.parent.end));
     }
+    // console.log(fileContent.substring(node.pos, node.end), node.kind)
     ts.forEachChild(node, changesVisitor)
 }
 
 ts.forEachChild(sourceFile, changesVisitor)
 
+console.log(count)
