4c8a066191f4da9f63ee4105c9c741ce34b1150e
vivomo <929992114@qq.com>
2021-07-19 04:27:14 +0800

    指定范围内生成整数的 另一种数据结构算法

--- nodejs/test/createNoRepeatRandom.js
+++ nodejs/test/createNoRepeatRandom.js
@@ -13,7 +13,31 @@ let createNoRepeatRandom = (count, max) => {
 }
 
 
+let createNoRepeatRandom2 = (count, max) => {
+    if (count > max) {
+        throw 'invalid args'
+    }
+    let map = new Map();
+    let result = Array(count);
+    for (let i = 0; i < count; i++) {
+        let ranInt = ~~(Math.random() * max);
+        let temp = result[i] ??= i;
+        if (ranInt < count) {
+            result[i] = result[ranInt] ??= ranInt;
+            result[ranInt] = temp;
+        } else {
+            temp = map.get(ranInt) ?? ranInt;
+            map.set(ranInt, result[i])
+            result[i] = temp;
+        }
+    }
+    return result;
+}
+
 console.log(createNoRepeatRandom(5, 10));
 console.log(createNoRepeatRandom(7, 10));
 console.log(createNoRepeatRandom(9, 10));
 
+console.log(createNoRepeatRandom2(5, 10));
+console.log(createNoRepeatRandom2(7, 10));
+console.log(createNoRepeatRandom2(9, 10));
