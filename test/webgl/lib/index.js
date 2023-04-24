44dbc8bca9e7035a537c8468fc7b4a9574cca578
vivomo <929992114@qq.com>
2023-04-24 22:53:49 +0800

    feat: webgl 工具函数

--- test/webgl/lib/index.js
+++ test/webgl/lib/index.js
@@ -44,3 +44,109 @@ const getRotateMatrix = deg => new Float32Array([
   0.0, 0.0, 1.0, 0.0,
   0.0, 0.0, 0.0, 1,
 ]);
+
+// 矩阵复合函数
+const mixMatrix = (A, B) => {
+  const result = new Float32Array(16);
+
+  for (let i = 0; i < 4; i++) {
+    result[i] = A[i] * B[0] + A[i + 4] * B[1] + A[i + 8] * B[2] + A[i + 12] * B[3]
+    result[i + 4] = A[i] * B[4] + A[i + 4] * B[5] + A[i + 8] * B[6] + A[i + 12] * B[7]
+    result[i + 8] = A[i] * B[8] + A[i + 4] * B[9] + A[i + 8] * B[10] + A[i + 12] * B[11]
+    result[i + 12] = A[i] * B[12] + A[i + 4] * B[13] + A[i + 8] * B[14] + A[i + 12] * B[15]
+  }
+
+  return result;
+};
+
+// 归一化函数
+const normalized = arr => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] * arr[i]
+  }
+
+  const middle = Math.sqrt(sum);
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] / middle;
+  }
+};
+
+// 叉积函数 获取法向量
+const cross = (a,b) => new Float32Array([
+  a[1] * b[2] - a[2] * b[1],
+  a[2] * b[0] - a[0] * b[2],
+  a[0] * b[1] - a[1] * b[0],
+]);
+
+// 点积函数 获取投影长度
+const dot = (a, b) => a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+
+// 向量差
+const minus = (a, b) => new Float32Array([
+  a[0] - b[0],
+  a[1] - b[1],
+  a[2] - b[2],
+]);
+
+// 视图矩阵获取
+const getViewMatrix = (eyex, eyey, eyez, lookAtx, lookAty, lookAtz, upx, upy, upz) => {
+
+  // 视点
+  const eye = new Float32Array([eyex, eyey, eyez])
+  // 目标点
+  const lookAt = new Float32Array([lookAtx, lookAty, lookAtz])
+  // 上方向
+  const up = new Float32Array([upx, upy, upz])
+
+  // 确定z轴
+  const z = minus(eye, lookAt);
+
+  normalized(z);
+  normalized(up);
+
+  // 确定x轴
+  const x = cross(z, up);
+
+  normalized(x);
+  // 确定y轴
+  const y = cross(x, z);
+
+  return new Float32Array([
+    x[0],       y[0],       z[0],       0,
+    x[1],       y[1],       z[1],       0,
+    x[2],       y[2],       z[2],       0,
+    -dot(x,eye),-dot(y,eye),-dot(z,eye),1
+  ])
+};
+
+// 获取正射投影矩阵
+const getOrtho = (l, r, t, b, n, f) => new Float32Array([
+  2 / (r - l), 0, 0, 0,
+  0, 2 / (t - b), 0, 0,
+  0, 0, -2 / (f - n), 0,
+  -(r + l) / (r - l), -(t + b) / (t - b), -(f + n) / (f - n), 1
+]);
+
+// 获取透视投影矩阵
+const getPerspective = (fov, aspect, far, near) => {
+  fov = fov * Math.PI / 180;
+  return new Float32Array([
+    1/(aspect*Math.tan(fov / 2)), 0, 0, 0,
+    0, 1/(Math.tan(fov/2)),0,0,
+    0,0,-(far+near)/(far-near),-(2*far*near)/(far-near),
+    0,0,-1,0,
+  ])
+};
+
+const distanceSelf = (a, b) => {
+  const x = a[0] - b[0]
+  const y = a[1] - b[1]
+  const z = a[2] - b[2]
+
+  const v = x * x + y * y + z * z;
+
+  return Math.sqrt(v)
+};
